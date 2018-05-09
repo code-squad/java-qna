e5c2943c59442dbc9b7cc83f198c6aad97363176
imjinbro <jinbro@likelion.org>
2018-05-10 00:42:35 +0900

    add signup and redirect user list

--- src/main/java/codesquad/controller/UserController.java
+++ src/main/java/codesquad/controller/UserController.java
@@ -1,15 +1,25 @@
 package codesquad.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+
 @Controller
 public class UserController {
+    private ArrayList<User> users = new ArrayList<>();
 
     @RequestMapping(value="/user/create", method=RequestMethod.POST)
     public String create(User user) {
-        System.out.println(user);
-        return "index";
+        users.add(user);
+        return "redirect:/user/list";
+    }
+
+    @RequestMapping("/user/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "/user/list";
     }
 }

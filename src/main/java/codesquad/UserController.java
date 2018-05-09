0d5a6bef7ff2aea3b6cc27c12f988606245e052b
krapeaj <jaeepark@gmail.com>
2018-05-09 17:21:14 +0900

    Create user DTO and complete test run

--- src/main/java/codesquad/UserController.java
+++ src/main/java/codesquad/UserController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     @PostMapping("/create")
-    public String create(String userId, String password, String name, String email, Model model){
-        model.addAttribute("userId", userId);
-        model.addAttribute("password", password);
-        model.addAttribute("name", name);
-        model.addAttribute("email", email);
+    public String create(UserDTO userDTO, Model model){
+        model.addAttribute("userId", userDTO.getUserId());
+        model.addAttribute("password", userDTO.getPassword());
+        model.addAttribute("name", userDTO.getName());
+        model.addAttribute("email", userDTO.getEmail());
         return "hello";
     }
 }

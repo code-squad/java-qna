1550eec6e2455f06acf24e786c6681dd9b522826
durin93 <lsc401@naver.com>
2018-05-09 19:15:35 +0900

    step2

--- src/main/java/codesquad/domain/User.java
+++ src/main/java/codesquad/domain/User.java
@@ -1,8 +1,20 @@
-package codesquad.web;
+package codesquad.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(nullable = false, length = 20)
 	private String userId;
+
 	private String password;
 	private String name;
 	private String email;
@@ -39,11 +51,15 @@ public class User {
 		this.email = email;
 	}
 
+	public void update(User newUser) {
+		this.password = newUser.password;
+		this.name = newUser.name;
+		this.email = newUser.email;
+	}
+
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
 	}
 
-	
-	
 }
--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -1,70 +1,64 @@
 package codesquad.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import codesquad.domain.User;
+import codesquad.domain.UserRepository;
 
 @Controller
 public class UserController {
 
-	private List<User> users = new ArrayList<>();
+	@Autowired
+	private UserRepository userRepository;
 
-	
-	
-	@GetMapping("/user/checkForm")
-	public String checkForm() {
-		return "checkUser.html";
+	@GetMapping("/user/form")
+	public String userForm() {
+		return "/user/form";
 	}
-	
-	@GetMapping("/user/checkUser")
-	public String checkUser(String userId, String password, Model model) {
-		for(int i = 0; i <users.size(); i++) {
-			if(users.get(i).getUserId().equals(userId))
-			{
-				if(users.get(i).getPassword().equals(password)) {
-					model.addAttribute("userId", userId);
-					model.addAttribute("index", i);
-					return "updateForm";
-				}
-			}
-		}
-		return "/user/checkForm";
+
+	@GetMapping("/users/{id}/form")
+	public String userUpdateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "/user/updateForm";
 	}
-	
-	@PostMapping("/user/update")
-	public String update(User user, String index) {
-		users.set(Integer.parseInt(index),user);
-		return "redirect:/user/list";
-	}
-	
+
 	@PostMapping("/user/create")
 	public String create(User user) {
 		System.out.println(user);
-		users.add(user);
+		userRepository.save(user);
+		return "redirect:/user/list";
+	}
+
+	@PutMapping("/user/update/{id}")
+	public String userUpdate(@PathVariable Long id, User newUser) {
+		User user = userRepository.findById(id).get();
+		user.update(newUser);
+		userRepository.save(user);
 		return "redirect:/user/list";
 	}
 
 	@GetMapping("/user/list")
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
 	}
+	
+	
 
-	@GetMapping("/users/{userId}")
-	public String profile(@PathVariable String userId, Model model) {
-		User user = null;
-		for (int i = 0; i < users.size(); i++) {
-			if (users.get(i).getUserId().equals(userId)) {
-				user = users.get(i);
-			}
-		}
-
+	@GetMapping("/user/profile/{id}")
+	public String profile(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).get();
 		model.addAttribute("user", user);
-		return "profile";
+		return "/user/profile";
 	}
+	
+	
+
 }

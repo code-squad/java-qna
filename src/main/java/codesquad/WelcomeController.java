07c218e1c8c4926d52b6df012ea609ee650816d6
krapeaj <jaeepark@gmail.com>
2018-05-09 15:37:06 +0900

    Add welcome controller

---
+++ src/main/java/codesquad/WelcomeController.java
@@ -0,0 +1,16 @@
+package codesquad;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+
+    @GetMapping("/helloworld")
+    public String welcome(String name, Model model){
+        model.addAttribute(name);
+        return "hello";
+    }
+}

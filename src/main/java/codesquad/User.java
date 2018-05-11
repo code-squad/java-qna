45c2d80eb378db10bdc9296ddfa01221b4c330af
krapeaj <jaeepark@gmail.com>
2018-05-11 17:10:17 +0900

    Complete mapping userId to profile.html

--- src/main/java/codesquad/User.java
+++ src/main/java/codesquad/User.java
@@ -37,4 +37,8 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public boolean isMatch(String userId) {
+        return this.userId.equals(userId);
+    }
 }
--- src/main/java/codesquad/UserController.java
+++ src/main/java/codesquad/UserController.java
@@ -3,6 +3,7 @@ package codesquad;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -13,14 +14,27 @@ public class UserController {
     List<User> users = new ArrayList<>();
 
     @PostMapping("/create")
-    public String create(User user){
+    public String create(User user) {
         users.add(user);
-        return "redirect:/list";
+        return "redirect:/user/list";
     }
 
-    @GetMapping("/list")
-    public String list(Model model){
+    @GetMapping("/user/list")
+    public String list(Model model) {
         model.addAttribute("users", users);
-        return "list";
+        return "/user/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String profile(@PathVariable String userId, Model model) {
+
+        for (User user : users) {
+            if (user.isMatch(userId)) {
+                model.addAttribute("user", user);
+                break;
+            }
+        }
+
+        return "/user/profile";
     }
 }

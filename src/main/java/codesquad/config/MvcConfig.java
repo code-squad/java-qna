bfeeda7c4a4266ae656344a3063b464223a27573
krapeaj <jaeepark@gmail.com>
2018-05-12 15:24:06 +0900

    Comment out unwanted html code and fix minor bug

--- src/main/java/codesquad/config/MvcConfig.java
+++ src/main/java/codesquad/config/MvcConfig.java
@@ -13,6 +13,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
         registry.addViewController("/user/form").setViewName("/user/form");
         registry.addViewController("/user/login").setViewName("/user/login");
-        registry.addViewController("/qna/form").setViewName("/qna/form");
+        registry.addViewController("/question").setViewName("/qna/form");
     }
 }

b58f72a8175359ea25c5efcf521d43feb932ebad
durin93 <lsc401@naver.com>
2018-05-15 00:24:28 +0900

    add testCode

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -13,11 +13,17 @@ public class UserTest {
 	@Before
 	public void setUp() {
 		user = new User();
+		user.setId((long)1);
 		user.setName("이그램");
 		user.setPassword("1234");
 		user.setEmail("gram@codesqaud");
 	}
 
+	@Test
+	public void matchIdTest() {
+		assertThat(user.matchId((long)1), is(true));
+	}
+	
 	@Test
 	public void matchPasswordTest() {
 		assertThat(user.matchPassword("1234"), is(true));

b4c99afc9142f4bc2e2f3eda578c2a5f337644ac
larryjung <corbswjd@gmail.com>
2018-05-15 14:51:32 +0900

    사용자 프로필 수정 구현

--- src/main/java/codesquad/web/controller/UserController.java
+++ src/main/java/codesquad/web/controller/UserController.java
@@ -23,7 +23,6 @@ public class UserController {
     // post는 받아서 전달
     @PostMapping("create")
     public String create(User user) {
-        System.out.println("User : " + user);
         userRepository.save(user);
         users.addUser(user);
         return "redirect:/users"; // 얘는 get맵핑이 되어 있어야 함.
@@ -33,32 +32,30 @@ public class UserController {
     @GetMapping("")
     public String list(Model model) {
         model.addAttribute("users", userRepository.findAll());
-        System.out.println(users.toString());
         return "user/list";
     }
 
-    @GetMapping("{userId}")
-    public String showUser(Model model, @PathVariable("userId") String userId) {
-        User user = users.findUser(userId);
-        System.out.println("find user : " + user.getUserId());
-        model.addAttribute("user", user);
+    @GetMapping("{id}")
+    public String showUser(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userRepository.findOne((long) id));
         return "user/profile";
     }
 
     @GetMapping("{id}/form")
     public String showUpdatePage(Model model, @PathVariable("id") int id) {
-        User user = users.findUser(id);
-        System.out.println("find user : " + user.getUserId());
-        model.addAttribute("user", user);
+        model.addAttribute("user", userRepository.findOne((long) id));
         return "user/updateForm";
     }
 
     @PostMapping("update")
-    public String update(String userId, String beforePassword, String password, String name, String email) {
-        User user = users.matchUser(userId, beforePassword);
-        if (user == null) return "/";
-        user.update(password, name, email);
-        return "redirect:/users";
+    public String update(int id, String userId, String beforePassword, String password, String name, String email) {
+        User user = userRepository.findOne((long) id);
+        if(user.matchWith(beforePassword)) {
+            user.update(password, name, email);
+            userRepository.save(user);
+            return "redirect:/users";
+        }
+        return "/";
     }
 
 }

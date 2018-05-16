884cde5f25dbe6e3c785ef8b1ef081d8cc4c8e65
HYEON SEONG CHUN <godlovesjoe@naver.com>
2018-05-16 17:12:38 +0900

    edit profile and UserController

--- src/main/java/codesquad/codesquad/web/UserController.java
+++ src/main/java/codesquad/codesquad/web/UserController.java
@@ -4,7 +4,9 @@ import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +14,6 @@ import java.util.List;
 @Controller
 public class UserController {
     private List<User> users = new ArrayList<>();
-
     @PostMapping("/user/create")
     public String create(User user){
         System.out.println("user: " + user);
@@ -20,9 +21,20 @@ public class UserController {
         return "redirect:/list";
     }
 
-    @GetMapping("/list")
+    @GetMapping("list")
     public String list(Model model) {
         model.addAttribute("users", users);
         return "list";
     }
+
+    @GetMapping("/user/{userId}/profile")
+    public String profile(@PathVariable String userId ,Model model){
+        for (User user:users) {
+            if (user.getUserId().equals(userId)){
+                model.addAttribute("user", user);
+            }
+        }
+        return "profile";
+    }
+
 }

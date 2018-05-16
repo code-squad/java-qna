f06cd8c1c03e62579c4b99269e3892c144af7846
imjinbro <jinbro@likelion.org>
2018-05-16 12:31:06 +0900

    change userInfo http method(post -> put)

--- src/main/java/codesquad/controller/UserController.java
+++ src/main/java/codesquad/controller/UserController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -58,7 +55,7 @@ public class UserController {
         return "/user/edit";
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public String update(@PathVariable("id") String id, String currentPasswd, User updateInfo) {
         Optional<User> optionalUser = userRepo.findById(Long.valueOf(id));
         if (!optionalUser.isPresent()) {

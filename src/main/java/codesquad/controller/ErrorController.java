b63ad681a41c3660e41ac5f63fa6d1aec168d8f5
imjinbro <jinbro@likelion.org>
2018-05-23 02:29:13 +0900

    change error handle - UnAuthorizedException

--- src/main/java/codesquad/controller/ErrorController.java
+++ src/main/java/codesquad/controller/ErrorController.java
@@ -6,9 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -16,8 +16,10 @@ public class ErrorController {
     private static final Logger log = LoggerFactory.getLogger(ErrorController.class);
 
     @ExceptionHandler(UnAuthorizedException.class)
-    public ResponseEntity<?> handleUnAuthorizeRequest(UnAuthorizedException e) {
+    public ModelAndView handleUnAuthorizeRequest(UnAuthorizedException e) {
         log.error(" ERROR OCCUR : { } ", e.getMessage());
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        ModelAndView modelAndView = new ModelAndView("/error/err.html");
+        modelAndView.setStatus(HttpStatus.UNAUTHORIZED);
+        return modelAndView;
     }
 }

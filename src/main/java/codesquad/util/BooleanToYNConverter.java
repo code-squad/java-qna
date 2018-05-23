236167fd18ddf3545fd61ef504c258a4c8acbe30
imjinbro <jinbro@likelion.org>
2018-05-23 13:55:42 +0900

    change SessionInterceptor exclude URI pattern - signup page URI

--- src/main/java/codesquad/util/BooleanToYNConverter.java
+++ src/main/java/codesquad/util/BooleanToYNConverter.java
@@ -6,10 +6,12 @@ import javax.persistence.Converter;
 @Converter
 public class BooleanToYNConverter implements AttributeConverter<Boolean, String> {
 
+    @Override
     public String convertToDatabaseColumn(Boolean attribute) {
         return (attribute != null && attribute) ? "Y" : "N";
     }
 
+    @Override
     public Boolean convertToEntityAttribute(String s) {
         return "Y".equals(s);
     }

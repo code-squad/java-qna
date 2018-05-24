05f033ee05567c65cc3e9d003be495337322a536
jaeyeon kim <foodsksms@gmail.com>
2018-05-24 14:21:13 +0900

    step6 pr보내기전

--- src/main/java/codesquad/QnaApplication.java
+++ src/main/java/codesquad/QnaApplication.java
@@ -11,8 +11,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableSwagger2
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -72,4 +72,5 @@ function deleteAnswer(e) {
             }
         }
     });
+
 }

7a141a29b66798ac0522b2310cecb5ff767302ca
David <xodnjs2549@naver.com>
2018-05-30 20:13:09 +0900

    Feat : Handlebars Custom Helper 추가

--- src/main/java/codesquad/domain/CustomHelper.java
+++ src/main/java/codesquad/domain/CustomHelper.java
@@ -1,14 +1,10 @@
 package codesquad.domain;
 
-import com.github.jknack.handlebars.Helper;
-import com.github.jknack.handlebars.Options;
 import pl.allegro.tech.boot.autoconfigure.handlebars.HandlebarsHelper;
 
-import java.io.IOException;
-
 @HandlebarsHelper
-public class CustomHelper{
-    public String post() {
+public class CustomHelper {
+    public String please() {
         return "please!!";
     }
 }

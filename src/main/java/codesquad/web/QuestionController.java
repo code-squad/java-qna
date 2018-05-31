86fc1ffe01b6a70f3f278666c69dfe52dbf61cd5
HYEON SEONG CHUN <godlovesjoe@naver.com>
2018-05-31 16:49:58 +0900

    로그인 기능 완료

--- src/main/java/codesquad/web/QuestionController.java
+++ src/main/java/codesquad/web/QuestionController.java
@@ -2,6 +2,8 @@ package codesquad.web;
 
 import codesquad.domain.Question;
 import codesquad.domain.QuestionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class QuestionController {
-
+    
     @Autowired
     private QuestionRepository questionRepository;
 
--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -2,19 +2,54 @@ package codesquad.web;
 
 import codesquad.domain.User;
 import codesquad.domain.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
-
+    
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "/user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(String userId, String password, HttpSession session) {
+
+        User user = userRepository.findByUserId(userId);
+
+        if (user == null) {
+            log.debug("login failed by user name");
+            return "redirect:/users/loginForm";
+        }
+
+        if (!password.equals(user.getPassword())) {
+            log.debug("login failed by password");
+            return "redirect:/users/loginForm";
+        }
+
+        log.debug("login success!");
+        session.setAttribute("user", user);
+
+        return "redirect:/users/";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 
     @GetMapping("/join")
     public String join() {

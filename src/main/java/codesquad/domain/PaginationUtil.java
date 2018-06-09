63643f47c54064b98b97abc2ee056a5075632d57
David <xodnjs2549@naver.com>
2018-06-09 15:17:49 +0900

    Feat : 현재 페이지의 블록 번호를 구하는 기능 추가

--- src/main/java/codesquad/domain/PaginationUtil.java
+++ src/main/java/codesquad/domain/PaginationUtil.java
@@ -1,17 +1,26 @@
 package codesquad.domain;
 
 public class PaginationUtil {
-    public static int getStartpage(int totalPages, int currentPage) {
-        return (int)((((double)currentPage-1)/(double)totalPages)*(double)totalPages+1);
+    private static final int PAGE_BLOCK_SIZE = 5;
+    private static final int FIRST = 1;
+
+    public static int getBlockStartPage(int pageBlockSize, int currentPage) {
+        return ((currentPage - 1) / pageBlockSize) * pageBlockSize + 1;
     }
 
-
-
-    public static int getEndPage(int pageSize, int totalPages, int startPage) {
-        int endPage = startPage + pageSize - 1;
+    public static int getEndPage(int pageBlockSize, int totalPages, int startPage) {
+        int endPage = startPage + pageBlockSize - 1;
         if (endPage < totalPages) {
             return endPage;
         }
         return totalPages;
+    }
+
+    public static boolean isFirstBlock(int currentPage) {
+        return currentPage == FIRST;
+    }
+
+    public static int getBlockNumberCurrentPage(int currentPage, int blockSize) {
+        return (int)Math.ceil((currentPage - 1) / blockSize) + 1;
     }
 }
--- src/test/java/codesquad/domain/PaginationUtilTest.java
+++ src/test/java/codesquad/domain/PaginationUtilTest.java
@@ -6,19 +6,37 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class PaginationUtilTest {
+    private final int BLOCK_SIZE = 5;
+    private final int PAGE_SIZE = 5;
+
     @Test
     public void getStartPage() {
-        int totalPages = 15;
-        int currentPage = 6;
-        assertThat(PaginationUtil.getStartpage(totalPages, currentPage),is(6));
+        int BLOCK_SIZE = 5;
+        int currentPage = 23;
+        assertThat(PaginationUtil.getBlockStartPage(BLOCK_SIZE, currentPage),is(21));
     }
 
     @Test
     public void getEndPage() {
-        int totalPages = 15;
+        int totalPages = 18;
         int currentPage = 11;
-        int pageSize = 5;
-        int startPage = PaginationUtil.getStartpage(totalPages, currentPage);
-        assertThat(PaginationUtil.getEndPage(pageSize, totalPages, startPage),is(15));
+        int pageBlockSize = 5;
+        int startPage = PaginationUtil.getBlockStartPage(pageBlockSize, currentPage);
+        assertThat(PaginationUtil.getEndPage(pageBlockSize, totalPages, startPage),is(15));
+    }
+
+    @Test
+    public void isFirstBlock() {
+        int pageBlockSize = 5;
+        int currentPage = 4;
+        int startPage = PaginationUtil.getBlockStartPage(pageBlockSize, currentPage);
+        System.out.println(startPage);
+        assertThat(PaginationUtil.isFirstBlock(startPage), is(true));
+    }
+
+    @Test
+    public void getBlockNumberCurrentPage() {
+        int currentPage = 17;
+        assertThat(PaginationUtil.getBlockNumberCurrentPage(currentPage, BLOCK_SIZE),is(2));
     }
 }

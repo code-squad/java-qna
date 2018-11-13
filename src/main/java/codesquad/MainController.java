ba0f37759e3a10efab306ac1d199845af6290c22
jar100 <jar10038@gmail.com>
2018-11-13 20:26:52 +0900

    qna 생성과 뷰

--- src/main/java/codesquad/MainController.java
+++ src/main/java/codesquad/MainController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
+/*    @GetMapping("/")
     public String create() {
         return "index";
-    }
+    }*/
 }
 
--- src/main/java/codesquad/question/QuestionController.java
+++ src/main/java/codesquad/question/QuestionController.java
@@ -1,3 +1,4 @@
+/*
 package codesquad.question;
 
 import org.springframework.stereotype.Controller;
@@ -21,6 +22,50 @@ public class QuestionController {
     public String list() {
 
         return "/";
+    }
+
+}
+*/
+
+package codesquad.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class QuestionController {
+    private static List<Question> questions = new ArrayList<>();
+    static {
+        Question question1 = new Question();
+        Question question2 = new Question();
+        question1.setTitle("qqqq");
+        question2.setTitle("aaaa");
+        question1.setWriter("pobi");
+        question2.setWriter("java");
+        question1.setContents("123");
+        question2.setContents("qweasd");
+        questions.add(question1);
+        questions.add(question2);
+    }
+
+    @PostMapping("/questions")
+    public String questions(Question question) {
+        System.out.println(question);
+        System.out.println("AAA");
+        questions.add(question);
+        return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String list(Model model) {
+        System.out.println("getquestions");
+        model.addAttribute("questions",questions);
+        return "index";
     }
 
 }

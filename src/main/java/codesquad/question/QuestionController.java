da8d8465ddcab58872cd0961868660175f8248b9
Brad Lee <brad903@naver.com>
2018-11-14 15:40:13 +0900

    Controller내 Mapping url주소 중복 처리 및 home 리다이렉팅

--- src/main/java/codesquad/question/QuestionController.java
+++ src/main/java/codesquad/question/QuestionController.java
@@ -1,39 +1,40 @@
 package codesquad.question;
 
-import org.aspectj.weaver.ast.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
+@RequestMapping("/questions")
 public class QuestionController {
     private List<Question> questions = TestQuestions.addQuestions();
 
-    @GetMapping("/")
+    @GetMapping
     public String home(Model model) {
-        Collections.reverse(questions);
-        model.addAttribute("questions", questions);
+        Comparator<Question> comp = (q1, q2) -> -1;
+        List sortedQuestions = questions.stream().sorted(comp.reversed()).collect(Collectors.toList());
+        model.addAttribute("questions", sortedQuestions);
         return "index";
     }
 
-    @PostMapping("/questions")
+    @PostMapping
     public String question(Question question) {
         question.setTime(getTodayDate());
         question.setIndex(questions.size() + 1);
         questions.add(question);
-        return "redirect:/";
+        return "redirect:/questions";
     }
 
-    @GetMapping("/questions/{index}")
+    @GetMapping("/{index}")
     public String detail(@PathVariable int index, Model model) {
+        System.out.println("index : " + index);
         Question theQuestion = questions.get(index - 1);
         model.addAttribute("theQuestion", theQuestion);
         return "qna/show";
--- src/main/java/codesquad/user/UserController.java
+++ src/main/java/codesquad/user/UserController.java
@@ -5,42 +5,44 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     private List<User> users = TestUsers.addUsers();
 
-    @PostMapping("/users")
-    public String create(User user) {  // 매번 요청될 때마다 SpringFramework에서 만들어줘
+    @PostMapping
+    public String create(User user) {
         users.add(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public String list(Model model) {
         model.addAttribute("users", users);
         return "user/list";
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     public String showProfile(@PathVariable String userId, Model model) {
         User user = users.stream().filter(users -> users.isUserId(userId)).findAny().get();
         model.addAttribute("user", user);
         return "user/profile";
     }
 
-    @GetMapping("/users/{userId}/form")
+    @GetMapping("/{userId}/form")
     public String showUserInfo(@PathVariable String userId, Model model) {
         User user = users.stream().filter(users -> users.isUserId(userId)).findAny().get();
         model.addAttribute("user", user);
         return "user/updateForm";
     }
 
-    @PostMapping("/users/{userId}")
+    @PostMapping("/{userId}")
     public String updateUserInfo(@PathVariable String userId, User userUpdated) {
         users = users.stream()
                 .map(theUser -> theUser.isUserId(userId)? userUpdated : theUser)

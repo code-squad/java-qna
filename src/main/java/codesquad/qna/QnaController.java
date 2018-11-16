ed153e0272c51d4bfa602b8f2f908e882f55aa7e
LAPTOP-E3LTPJD6\ChoiSing <chltmdals115@gmail.com>
2018-11-16 17:03:52 +0900

    step1, complete

--- src/main/java/codesquad/qna/QnaController.java
+++ src/main/java/codesquad/qna/QnaController.java
@@ -5,26 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/qna")
 public class QnaController {
     private List<Question> questions = new ArrayList<>();
-    @PostMapping("/qna/create")
+    @PostMapping("/create")
     public String create(Question question){
         questions.add(question);
         return "redirect:/";
     }
 
-    @GetMapping("/")
+    @GetMapping
     public String list(Model model){
         model.addAttribute("questions", questions);
         return "index";
     }
 
-    @GetMapping("/qna/{index}")
+    @GetMapping("/{index}")
     public String show(@PathVariable int index, Model model){
         model.addAttribute("question", questions.get(index-1));
         return "qna/show";
--- src/main/java/codesquad/user/UserController.java
+++ src/main/java/codesquad/user/UserController.java
@@ -5,49 +5,60 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-    private List<User> users = new ArrayList();
+    private List<User> users = new ArrayList<>();
 
-    @PostMapping("/users/create")
+    @PostMapping("/create")
     public String create(User user){
         users.add(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
-    public String list(Model model){
+    @GetMapping
+    public String showUsers(Model model){
         model.addAttribute("users", users);
         return "user/list";
     }
 
-    @GetMapping("/users/{userId}")
-    public String profile(@PathVariable String userId, Model model){
+    @GetMapping("/{userId}")
+    public String showProfile(@PathVariable String userId, Model model){
         setUpUser(userId, model);
         return "user/profile";
     }
 
-    public Model setUpUser(String userId, Model model){
+    private Model setUpUser(String userId, Model model){
         User user = findUserById(userId);
         model.addAttribute("user", user);
         return model;
     }
 
-    public User findUserById(String userId){
+    private User findUserById(String userId){
         for (User user : users) {
             if(user.isUser(userId)) return user;
         }
         return null;
     }
 
-    @GetMapping("/users/{userId}/form")
-    public String modify(@PathVariable String userId, Model model){
+    @GetMapping("/{userId}/form")
+    public String showModifyView(@PathVariable String userId, Model model){
         setUpUser(userId, model);
-        System.out.println("?");
         return "user/updateForm";
+    }
+
+    private int findIndexById(String userId){
+        return users.indexOf(findUserById(userId));
+    }
+
+    @PostMapping("/{userId}/update")
+    public String updateUser(@PathVariable String userId, User user){
+        users.set(findIndexById(userId), user);
+        return "redirect:/users";
     }
 }

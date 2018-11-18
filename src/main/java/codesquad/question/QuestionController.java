fe803a206f71f10150c1b6fd8fcff0a47c635f9d
unknown <dud9203@gmail.com>
2018-11-18 18:17:32 +0900

    step2

--- src/main/java/codesquad/question/QuestionController.java
+++ src/main/java/codesquad/question/QuestionController.java
@@ -1,5 +1,6 @@
 package codesquad.question;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,27 +8,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
-    private QuestionRepository questions = QuestionRepository.getInstance();
+    @Autowired
+    QuestionRepository questionRepository;
 
     @GetMapping
     public String list(Model model) {
-        model.addAttribute("questions", questions.getAll());
+        model.addAttribute("questions", questionRepository.findAll());
         return "index";
     }
 
     @PostMapping
     public String create(Question question) {
-        questions.add(question);
+        questionRepository.save(question);
         return "redirect:/questions";
     }
 
     @GetMapping("/{index}")
-    public String show(Model model, @PathVariable int index) {
-        Question question = questions.get(index);
-        model.addAttribute("question", question);
-        return "/qna/show";
+    public String show(Model model, @PathVariable long index) {
+        Optional<Question> question = questionRepository.findById(index);
+        if (question.isPresent()) {
+            model.addAttribute("question", question.get());
+            return "/qna/show";
+        }
+        return "redirect:/questions";
     }
 }
--- src/main/java/codesquad/question/QuestionRepository.java
+++ src/main/java/codesquad/question/QuestionRepository.java
@@ -1,29 +1,7 @@
 package codesquad.question;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
-public class QuestionRepository {
-    private static QuestionRepository ourInstance = new QuestionRepository();
-    private List<Question> questions = new ArrayList<>();
-
-    public static QuestionRepository getInstance() {
-        return ourInstance;
-    }
-
-    private QuestionRepository() {
-    }
-
-    public void add(Question question) {
-        question.setIndex(questions.size() + 1);
-        questions.add(question);
-    }
-
-    public Question get(int index) {
-        return questions.get(index - 1);
-    }
-
-    public List<Question> getAll() {
-        return questions;
-    }
+public interface QuestionRepository extends CrudRepository<Question, Long> {
 }
+

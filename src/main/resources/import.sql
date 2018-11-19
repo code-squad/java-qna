5be625bfa5c102fc46ed6492347dda04de6751c3
zingoworks <zingoworks@gmail.com>
2018-11-19 17:00:23 +0900

    feat: add Question test cases

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO USER (id, user_id, password, name, email) VALUES (1, 'zingo', 'test', '징고', 'zingoworks@slipp.net');
 INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'vamboo', 'test', '뱀부', 'vamboo26@slipp.net');
+
+INSERT INTO QUESTION (id, writer, title, contents) VALUES (1, 'zingo', '징고의 질문:테스트', '이건 내용입니다.');
+INSERT INTO QUESTION (id, writer, title, contents) VALUES (2, 'vamboo', '뱀부의 질문:테스트', '저건 내용입니다.');

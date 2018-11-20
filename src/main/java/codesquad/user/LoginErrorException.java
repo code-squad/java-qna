96d036707aab029d2389744fcf64c15df83f9b10
Brad Lee <brad903@naver.com>
2018-11-20 10:39:15 +0900

    로그인 실패시 예외처리 대신 리다이렉트, 로그아웃 기능 구현

--- src/main/java/codesquad/user/LoginErrorException.java
+++
@@ -1,7 +0,0 @@
-package codesquad.user;
-
-public class LoginErrorException extends RuntimeException {
-    public LoginErrorException(String message) {
-        super(message);
-    }
-}
--- src/main/java/codesquad/user/UserController.java
+++ src/main/java/codesquad/user/UserController.java
@@ -51,12 +51,20 @@ public class UserController {
     @PostMapping("/login")
     public String login(String userId, String password, HttpSession session) {  // 전달하는 인자가 많지않을떄는 User 대신 이렇게 하나씩 받을 수 있음
         Optional<User> maybeUser = userRepository.findByUserId(userId);  // findByUserId 만들어야 할 필요성!
-        if (maybeUser.isPresent() && maybeUser.get().matchPassword(password)) {  // session을 통해 로그인한 정보를 클라이언트에게 보냄
+        if (maybeUser.isPresent()) {  // session을 통해 로그인한 정보를 클라이언트에게 보냄
             User user = maybeUser.get();
-            session.setAttribute("loginUser", user);  // 서버가 세션 아이디 기억함, 톰캣 서버상에 파일로 저장
-            return "redirect:/";
+            if(user.matchPassword(password)) {
+                session.setAttribute("loginUser", user);  // 서버가 세션 아이디 기억함, 톰캣 서버상에 파일로 저장
+                return "redirect:/";
+            }
         }
-        throw new LoginErrorException("아이디 또는 비밀번호가 일치하지 않습니다");
+        return "user/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("loginUser");
+        return "redirect:/";
     }
 
     private User getUser(@PathVariable Long id) {

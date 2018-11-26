98c16173e0c291c30a3a78d14f3440c81cb987a4
zingoworks <zingoworks@gmail.com>
2018-11-26 14:00:08 +0900

    feat: 로깅라이브러리 사용 세팅(at application.yml), SpringBoot HotSwap 세팅

--- src/main/java/codesquad/user/UserController.java
+++ src/main/java/codesquad/user/UserController.java
@@ -1,6 +1,8 @@
 package codesquad.user;
 
 import codesquad.HttpSessionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,12 +20,15 @@ import static codesquad.HttpSessionUtils.USER_SESSION_KEY;
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping
     public String create(User user) {
-        System.out.println("create user");
+        log.debug("create : {}", user);
+//        System.out.println("create user");
 
         userRepository.save(user);
         return "redirect:/users";
@@ -31,7 +36,8 @@ public class UserController {
 
     @GetMapping
     public String list(Model model) {
-        System.out.println("view user list");
+        log.debug("view user list");
+//        System.out.println("view user list");
 
         model.addAttribute("users", userRepository.findAll());
         return "/user/list";

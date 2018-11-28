f077436911b4470440f26ba184f3b946a0fa4169
Choi-dongsun <rovks91@gmail.com>
2018-11-28 22:10:36 +0900

    Answer Test Data 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,10 @@
 INSERT INTO USER (id, user_id, password, name, email) VALUES (1, 'finn', 'test', '동선', 'kokof21@naver.com');
 INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'pobi', 'test', '자바자기', 'javajigi@naver.com');
 
-INSERT INTO QUESTION (id, writer_id, title, contents, created_date, updated_date) VALUES (1, '1', '핀의 첫번째 질문', '핀의 첫번째 질문의 내용입니다.', current_timestamp, current_timestamp);
-INSERT INTO QUESTION (id, writer_id, title, contents, created_date, updated_date) VALUES (2, '1', '핀의 두번째 질문', '핀의 두번째 질문의 내용입니다.', current_timestamp, current_timestamp);
-INSERT INTO QUESTION (id, writer_id, title, contents, created_date, updated_date) VALUES (3, '2', '포비의 첫번째 질문', '포비의 첫번째 질문의 내용입니다.', current_timestamp, current_timestamp);
+INSERT INTO QUESTION (id, writer_id, title, contents, created_date, updated_date) VALUES (1, 1, '핀의 첫번째 질문', '핀의 첫번째 질문의 내용입니다.', current_timestamp, current_timestamp);
+INSERT INTO QUESTION (id, writer_id, title, contents, created_date, updated_date) VALUES (2, 1, '핀의 두번째 질문', '핀의 두번째 질문의 내용입니다.', current_timestamp, current_timestamp);
+INSERT INTO QUESTION (id, writer_id, title, contents, created_date, updated_date) VALUES (3, 1, '포비의 첫번째 질문', '포비의 첫번째 질문의 내용입니다.', current_timestamp, current_timestamp);
+
+INSERT INTO ANSWER (id, writer_id, question_id, contents, created_date, updated_date) VALUES (1, 1, 2, '핀의 두번째 질문글에 대한 핀의 코멘트입니다.', current_timestamp, current_timestamp);
+INSERT INTO ANSWER (id, writer_id, question_id, contents, created_date, updated_date) VALUES (2, 2, 2, '핀의 두번째 질문글에 대한 포비의 추가 코멘트입니다.', current_timestamp, current_timestamp);
+INSERT INTO ANSWER (id, writer_id, question_id, contents, created_date, updated_date) VALUES (3, 2, 1, '핀의 첫번째 질문글에 대한 포비의 코멘트입니니다.', current_timestamp, current_timestamp);

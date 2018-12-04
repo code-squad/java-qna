c1f5f909e8d83b856daa186f6d0025a25fdb87a8
zingoworks <zingoworks@gmail.com>
2018-12-04 09:36:27 +0900

    feat:답변삭제기능 추가(on function 활용 이전)

--- src/main/java/codesquad/answer/ApiAnswerController.java
+++ src/main/java/codesquad/answer/ApiAnswerController.java
@@ -1,6 +1,7 @@
 package codesquad.answer;
 
 import codesquad.HttpSessionUtils;
+import codesquad.Result;
 import codesquad.question.Question;
 import codesquad.question.QuestionRepository;
 import codesquad.user.User;
@@ -24,10 +25,30 @@ public class ApiAnswerController {
             return null;
         }
 
-        User loginUser = HttpSessionUtils.getUserFromSession(session);
+        User loggedInUser = HttpSessionUtils.getUserFromSession(session);
         Question question = questionRepository.findById(questionId).orElse(null);
-        Answer answer = new Answer(question, loginUser, contents);
+        Answer answer = new Answer(question, loggedInUser, contents);
         question.addAnswer();
+        questionRepository.save(question);
         return answerRepository.save(answer);
+    }
+
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Long questionId, @PathVariable Long id, HttpSession session) {
+        if (!HttpSessionUtils.isLoggedInUser(session)) {
+            return Result.fail("로그인이 필요합니다.");
+        }
+
+        Answer answer = answerRepository.findById(id).orElse(null);
+        User loggedInUser = HttpSessionUtils.getUserFromSession(session);
+        if(!answer.isMatchWriter(loggedInUser)) {
+            return Result.fail("작성자만 삭제가능합니다.");
+        }
+
+        Question question = questionRepository.findById(questionId).orElse(null);
+        question.deleteAnswer();
+        questionRepository.save(question);
+        answerRepository.deleteById(id);
+        return Result.ok();
     }
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -21,7 +21,7 @@ function addAnswer(e) {
         success : function (data, status) {
             console.log(data);
             var answerTemplate = $("#answerTemplate").html();
-            var template = answerTemplate.format(data.writer.name, data.formattedCreateDate, data.contents, data.question.id);
+            var template = answerTemplate.format(data.writer.name, data.formattedCreateDate, data.contents, data.question.id, data.id);
             $(".qna-comment-slipp-articles").append(template);
             $("textarea[name=contents]").val("");
         }
@@ -33,8 +33,28 @@ $("a.link-delete-article").click(deleteAnswer);
 function deleteAnswer(e) {
     e.preventDefault();
 
+    //this가 어디서 호출되느냐에 따라 달라져서 여기서 지정해두고 써야
+    var deleteBtn = $(this);
     var url = $(this).attr("href");
     console.log("url : " + url);
+
+    $.ajax({
+        type : 'delete',
+        url : url,
+        dataType : 'json',
+        error : function(xhr, status) {
+            console.log("error");
+        },
+        success : function(data, status) {
+            console.log(data);
+            if(data.valid) {
+            //자기자신으로부터 가장 가까운 article을 찾아가기
+                deleteBtn.closest("article").remove();
+            } else {
+                alert(data.errorMessage);
+            }
+        }
+    });
 }
 
 String.prototype.format = function() {

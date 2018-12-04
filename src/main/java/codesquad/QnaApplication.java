25709cf7ea7074a1d357356433980432d08fdfdb
LAPTOP-E3LTPJD6\ChoiSing <chltmdals115@gmail.com>
2018-12-04 18:02:55 +0900

    apply log lib

--- src/main/java/codesquad/QnaApplication.java
+++ src/main/java/codesquad/QnaApplication.java
@@ -1,13 +1,19 @@
 package codesquad;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class QnaApplication {
+    private static final Logger logger = getLogger(QnaApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(QnaApplication.class, args);
+        logger.debug("hello World");
     }
 }

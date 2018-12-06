168ef75e5472f2e60b52a90849ed0e1b680f4e80
kkssry <kkssry@naver.com>
2018-12-06 16:26:51 +0900

    수정시간 구현

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,7 +3,7 @@ INSERT INTO USER (id, user_id, password, name, email, CREATE_DATE) VALUES (2, 's
 INSERT INTO USER (id, user_id, password, name, email, CREATE_DATE) VALUES (3, 'kkssry', '1234', 'skull', 'kkssry@slipp.net',CURRENT_TIMESTAMP);
 INSERT INTO USER (id, user_id, password, name, email, CREATE_DATE) VALUES (4, 'aaa', 'aaa', 'qwe', 'asdf@slipp.net',CURRENT_TIMESTAMP);
 
-INSERT INTO QUESTION (id, writer_id, title, contents, create_date, count_of_answer, deleted) VALUES (1, 1, 'testTitle입니다.', 'testContent입니다.', CURRENT_TIMESTAMP,0, false);
-INSERT INTO QUESTION (id, writer_id, title, contents, create_date, count_of_answer, deleted) VALUES (2, 2, 'asdfasdf입니다.', 'qkhskjjasdff입니다.', CURRENT_TIMESTAMP,0,false);
-INSERT INTO QUESTION (id, writer_id, title, contents, create_date, count_of_answer, deleted) VALUES (3, 3, 'asdfasdf입니다.', 'qkhskjjasdff입니다.', CURRENT_TIMESTAMP,0,false);
+INSERT INTO QUESTION (id, writer_id, title, contents, create_date, count_of_answer, deleted, modified_date) VALUES (1, 1, 'testTitle입니다.', 'testContent입니다.', CURRENT_TIMESTAMP, 0, false ,CURRENT_TIMESTAMP);
+INSERT INTO QUESTION (id, writer_id, title, contents, create_date, count_of_answer, deleted, modified_date) VALUES (2, 2, 'asdfasdf입니다.', 'qkhskjjasdff입니다.', CURRENT_TIMESTAMP, 0, false ,CURRENT_TIMESTAMP);
+INSERT INTO QUESTION (id, writer_id, title, contents, create_date, count_of_answer, deleted, modified_date) VALUES (3, 3, 'asdfasdf입니다.', 'qkhskjjasdff입니다.', CURRENT_TIMESTAMP, 0, false ,CURRENT_TIMESTAMP);
 

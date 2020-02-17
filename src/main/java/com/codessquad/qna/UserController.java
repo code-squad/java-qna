81498c4a1848030a824b384d5f620faf633ee0e8
haveagood@naver.com <haveagood@naver.com>
2020-02-17 16:19:30 +0900

    fix : 호출 방식 변경
    - 기존의 GET방식으로 호출을 했을 경우 비밀번호가 노출되기 때문에(쿼리 노출?) POST메서드로 호출방식을 변경하였습니다.

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -3,10 +3,11 @@ package com.codessquad.qna;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-    @GetMapping("/user/create")
+    @PostMapping("/user/create")
     public String Create(String userId, String password, String name, String email, Model model) {
         model.addAttribute("userId",userId);
         model.addAttribute("password",password);

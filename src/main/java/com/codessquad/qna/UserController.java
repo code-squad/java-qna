751e4c1827bd64b0340c7a6bc9cb854f3b380950
han95210 <han95210@naver.com>
2020-02-17 16:11:52 +0900

    [step1 회원가입 기능 구현] get 방식의 url을 post 방식의 url로 바꾸는 과정 진행
    * UserContorller 클래스에서 @PostMapping구현
    * form.html 파일 78줄에서 method="get" 방식을 method="post" 방식으로 변경

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -1,4 +1,14 @@
 package com.codessquad.qna;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+    @PostMapping("/user/create")
+    public String create(String userId, String password, String name, String email) {
+        System.out.println("userId : " + userId + " name : " + name);
+        return "index";
+    }
 }

b290aad9ed3a3bf659b664e225b0819fdce16166
IdionKim <ksundong@gmail.com>
2020-02-17 16:30:44 +0900

    Add: User 정보를 출력하는 기능 추가
    PathVariable에 대해서 공부할 수 있었습니다.
    이름으로 조회하라고 하셨는데, HashMap이 아니라 ArrayList여서 Index값으로 조회하도록 수정해보았습니다.

--- src/main/java/com/codessquad/qna/user/UserController.java
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -3,6 +3,7 @@ package com.codessquad.qna.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +35,12 @@ public class UserController {
     public String showUserList(Model model) {
         model.addAttribute("users", users);
         return "user/list";
+    }
+
+    @GetMapping("/users/{userIndex}")
+    public String showUserProfile(@PathVariable int userIndex, Model model) {
+        model.addAttribute("user", users.get(userIndex));
+        return "user/profile";
     }
 
 }

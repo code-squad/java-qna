495a2bb87532cb68d666314d84b9db7e8287c8eb
Ever <hsik0225@gmail.com>
2020-02-17 20:59:09 +0900

    회원가입, 사용자 목록 기능 구현

---
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -0,0 +1,27 @@
+package com.codessquad.qna;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    private List<User> users = new ArrayList<>();
+
+    @PostMapping("/user/create")
+    public String create(User user) {
+        System.out.println("user = " + user);
+        users.add(user);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "list";
+    }
+}
---
+++ src/main/java/com/codessquad/qna/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.codessquad.qna;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/hello")
+    public String welcome(String name, int age, Model model) {
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "welcome";
+    }
+}

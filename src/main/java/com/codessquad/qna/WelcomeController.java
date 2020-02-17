3f1c015e544a723e62d1c28f7ab0ce9821c3d0cc
wooody92 <pkower4@naver.com>
2020-02-17 17:43:12 +0900

    add UserController class
    static/user/form -> method="get" -> "post"로 변경했습니다.
    UserController 클래스에서 /user/form.html로부터 입력된 데이터들을 받아옵니다.
    /user/create -> return ""으로 페이지를 맵핑시니다.

---
+++ src/main/java/com/codessquad/qna/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.codessquad.qna;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/helloworld")
+    public String welcome(String name, Model model) {
+        System.out.println("name : " + name);
+        model.addAttribute("name", name);
+        return "welcome";
+    }
+}

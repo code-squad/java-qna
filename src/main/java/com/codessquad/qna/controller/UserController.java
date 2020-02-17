cc3fd372eee5bd6f1e7709e600b9023c36fc86b0
KIM DONG HWAN <quaint3@gmail.com>
2020-02-17 22:04:35 +0900

    Style : reformatting code

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -2,7 +2,9 @@ package com.codessquad.qna.controller;
 
 import com.codessquad.qna.domain.User;
 import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +15,14 @@ public class UserController {
   private static ArrayList<User> users = new ArrayList<>();
 
   @PostMapping(value = "/user/create")
-  public String createUser(@ModelAttribute User user) {
+  public List<User> createUser(@ModelAttribute User user) {
     users.add(user);
-    return "user";
+    return users;
   }
 
   @GetMapping(value = "/users")
-  public String showAllUsers(){
+  public String showAllUsers(Model model) {
+    model.addAttribute("users", users);
     return "user/list";
   }
 

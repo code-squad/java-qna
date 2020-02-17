7833670788a0b574a6512f3ea9d8feba5ad0855a
blossun <dnsngu@gmail.com>
2020-02-17 16:43:18 +0900

    사용자가 전달한 값을 User 클래스를 생성해 저장
    저장할 값이 많아질 경우 하나하나 받지 않고 클래스를 생성해서 값을 저장할 수 있다.
    회원가입할 때 전달한 값을 저장할 수 있는 필드를 생성한 후 setter와 getter 메소드를 생성

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     @PostMapping("/user/create")
-    public String addUser(Model model, String userId, String password, String name, String email) {
-        System.out.println("userId : " + userId + " password : " + password);
-        model.addAttribute("name", name);
+    public String addUser(Model model, User user) {
+        System.out.println("userId : " + user.getUserId() + " password : " + user.getPassword());
+        model.addAttribute("name", user.getName());
         return "createok"; //templates의 index.html 호출
     }
 }

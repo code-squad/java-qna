46133f331e3435f313d2fe298f9a49de3dcee94d
blossun <dnsngu@gmail.com>
2020-02-17 16:58:42 +0900

    사용자 목록 관리 ArrayList 생성
    사용자 정보를 관리하기 위해 Users 리스트에 회원가입시 마다 사용자 정보가 User 객체로 저장

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
+    List<User> Users = new ArrayList<>();
 
     @PostMapping("/user/create")
     public String addUser(Model model, User user) {
         System.out.println("userId : " + user.getUserId() + " password : " + user.getPassword());
         model.addAttribute("name", user.getName());
+        Users.add(user);
         return "createok"; //templates의 index.html 호출
     }
 }

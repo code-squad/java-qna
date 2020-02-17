25ee28c0cc2b3cc23ed0b7a58fd28e41989c7635
wooody92 <pkower4@naver.com>
2020-02-17 17:44:31 +0900

    add User class
    userId, password, name, email 데이터를 User 클래스를 통해 받아옵니다. User 클래스는 setter와 toString을 오버라이드하는 메서드를 갖고있습니다.

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
     @PostMapping("/user/create")
-    public String create(String userId, String password, String name, String email) {
-        System.out.println(" userId : " + userId +
-                " password : " + password +
-                " name : " + name +
-                " email : " + email);
+    public String create(User user) {
+        System.out.println("user => " + user);
         return "index";
     }
 }

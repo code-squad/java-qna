8b36ff5abf4e88980ccefb1fcaa3e0fe0457746b
IdionKim <ksundong@gmail.com>
2020-02-17 17:32:31 +0900

    Add: 질문하기 화면 및 질문등록 기능 추가
    요구사항을 충족시켰습니다.
    추가로 공통부분을 template 최상단으로 끌어올렸습니다.
    index page도 getMapping으로 requestMapping이 되도록 하였습니다.

---
+++ src/main/java/com/codessquad/qna/question/QuestionController.java
@@ -0,0 +1,35 @@
+package com.codessquad.qna.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class QuestionController {
+    List<Question> questions = new ArrayList<>();
+
+    @GetMapping("/")
+    public String goIndexPage() {
+        return "index";
+    }
+
+    @GetMapping("/qna/form")
+    public String goQnaForm() {
+        return "qna/form";
+    }
+
+    @PostMapping("/questions")
+    public String createQuestion(HttpServletRequest request) {
+        String writer = request.getParameter("writer");
+        String title = request.getParameter("title");
+        String contents = request.getParameter("contents");
+
+        Question newQuestion = new Question(writer, title, contents);
+        questions.add(newQuestion);
+        return "redirect:/";
+    }
+}

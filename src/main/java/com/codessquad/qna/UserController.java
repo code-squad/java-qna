d3d387e7f4208b61b155af05e7b91a219ea48507
han95210 <han95210@naver.com>
2020-02-18 13:28:55 +0900

    [step1 회원 프로필 정보보기 구현] 화면 왼쪽 상단 slipp 클릭시 오류 화면 해결
    * template에 있는 index.html / list.html / profile.html 의 소스 코드 수정
    * UserController index 메서드 수정

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -3,6 +3,7 @@ package com.codessquad.qna;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -12,11 +13,16 @@ import java.util.List;
 public class UserController {
     public List<User> users = new ArrayList<>();
 
+    @GetMapping("/index.html")
+    public String index() {
+        return "index";
+    }
+
     @PostMapping("/user/create")
     public String create(User user) {
         System.out.println("user : " + user);
         users.add(user);
-        return "redirect:/user/list.html";
+        return "redirect:list.html";
     }
 
     @GetMapping("/user/list.html")
@@ -24,4 +30,23 @@ public class UserController {
         model.addAttribute("users", users);
         return "list";
     }
+
+    @GetMapping("/user/profile.html")
+    public String profile(Model model) {
+        model.addAttribute("users", users);
+        return "profile";
+    }
+
+    @GetMapping("/user/{userId")
+    public String profileName(Model model) {
+
+        return "redirect:/profile";
+    }
+
+//    @GetMapping("/user/{userId}")
+//    public String profileName(@PathVariable String userId) {
+//        String returnName = "";
+//        if (userId.equals(""))
+//    }
+
 }

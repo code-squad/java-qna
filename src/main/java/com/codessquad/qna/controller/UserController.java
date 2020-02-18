25da0aa8f6122b68b4e7a6a54c856d2f5a5e4dd1
moonelysian <dandyblack@naver.com>
2020-02-18 17:10:10 +0900

    html 공통부분 directory로 묶기  - 한 눈에 모아보기 쉽게 모음

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.codessquad.qna.user;
 
-import com.codessquad.qna.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -24,5 +24,18 @@ public class UserController {
     public String getUserList(Model model) {
         model.addAttribute("users", users);
         return "user/list";
+    }
+
+    @GetMapping("/users/join")
+    public String signInForm() {
+        return "user/form";
+    }
+
+    @GetMapping("/users/{userIndex}")
+    public String getUserProfile(@PathVariable int userIndex, Model model) {
+        System.out.println(userIndex);
+       User user = users.get(userIndex);
+       model.addAttribute("user", user);
+       return "user/profile";
     }
 }

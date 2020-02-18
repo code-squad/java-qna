196728d64a8eea28f36eff970eae221780fe50ba
haveagood@naver.com <haveagood@naver.com>
2020-02-18 13:37:55 +0900

    feat : 회원 목록 기능 구현
    - 회원 가입이 완료 된 후 , 회원 목록으로 redirection 하는 기능 구현
    
    - 왼쪽 상단 사람 모양의 아이콘 클릭시 회원 목록으로 이동하는 기능 구현
    
    - 회원 가입을 완료했을 경우 회원 목록에 해당 회원의 userId, name, email을 출력하는 기능 구현

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -2,18 +2,24 @@ package com.codessquad.qna;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
-    @PostMapping("/user/create")
-    public String Create(User user, Model model) {
-        System.out.println(user.toString());
-        model.addAttribute("userId",user.getUserId());
-        model.addAttribute("password",user.getPassword());
-        model.addAttribute("name",user.getName());
-        model.addAttribute("email",user.getEmail());
-        return "CreateUser";
+    List<User> users = new ArrayList<>();
+
+    @PostMapping("/user/create.html")
+    public String create(User user, Model model) {
+        users.add(user);
+        return "redirect:/user/list.html";
+    }
+
+    @GetMapping("/user/list.html")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "/user/list";
     }
 }

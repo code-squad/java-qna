e3c20ede9089ac3134900c3e6d856e05bbdf10da
beemiel <qnswwrn@gmail.com>
2020-02-18 23:39:13 +0900

    Question <생성>: 질문을 저장할 클래스
    qna/form에서 입력 받은 내용들을 저장하기 위한 클래스

---
+++ src/main/java/com/codessquad/qna/Question.java
@@ -0,0 +1,50 @@
+package com.codessquad.qna;
+
+public class Question {
+    private String writer;
+    private String title;
+    private String contents;
+    private String time;
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "writer='" + writer + '\'' +
+                ", title='" + title + '\'' +
+                ", contents='" + contents + '\'' +
+                ", time='" + time + '\'' +
+                '}';
+    }
+}

093e3f41ed4f71a3aefd66cd9397b3c90ff6612d
kyungrae <rlarudfo93@gmail.com>
2020-02-18 18:20:52 +0900

    feat: 질문하기 기능 구현
    요구사항에 맞게 post-redirection-get 구조로 기능 구현

---
+++ src/main/java/com/codessquad/qna/web/QuestionController.java
@@ -0,0 +1,36 @@
+package com.codessquad.qna.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class QuestionController {
+    private List<Question> questions = new ArrayList<>();
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("questions", questions);
+        return "index";
+    }
+
+    @GetMapping("/questions/{index}")
+    public String question(@PathVariable int index, Model model) {
+        Question question = questions.get(index - 1);
+        model.addAttribute("question", question);
+        return "qna/show";
+    }
+
+    @PostMapping("/questions")
+    public String createQuestion(Question question) {
+        System.out.println(question);
+        questions.add(question);
+        return "redirect:/";
+    }
+
+}

339a3e1f2689a62d0c24a3e13560950dba0633b5
moonelysian <dandyblack@naver.com>
2020-02-18 14:45:27 +0900

    user 패키지 추가 - user class, user controller 같이 두기

---
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -0,0 +1,28 @@
+package com.codessquad.qna.user;
+
+import com.codessquad.qna.user.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    private List<User> users = new ArrayList<>();
+
+    @PostMapping("/users")
+    public String createUser(User user) {
+        System.out.println(user);
+        users.add(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String getUserList(Model model) {
+        model.addAttribute("users", users);
+        return "user/list";
+    }
+}

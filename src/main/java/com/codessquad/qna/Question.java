63e7ac58b323fe66e47a61de6c5aa913f52a18e3
beemiel <qnswwrn@gmail.com>
2020-02-18 23:59:50 +0900

    Question <수정>: 질문에 index 변수 추가
    각각의 질문들을 구별할 수 있는 index 변수 추가

--- src/main/java/com/codessquad/qna/Question.java
+++ src/main/java/com/codessquad/qna/Question.java
@@ -5,6 +5,15 @@ public class Question {
     private String title;
     private String contents;
     private String time;
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
 
     public String getWriter() {
         return writer;
@@ -45,6 +54,7 @@ public class Question {
                 ", title='" + title + '\'' +
                 ", contents='" + contents + '\'' +
                 ", time='" + time + '\'' +
+                ", index=" + index +
                 '}';
     }
 }

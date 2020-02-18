09762f779ca4723535676284b64d869bb8c0124d
beemiel <qnswwrn@gmail.com>
2020-02-18 21:57:57 +0900

    UserController <추가>: 프로필 보기 기능 추가
    - getUserProfile(): 유저의 프로필을 볼 수 있는 페이지와 매핑.
    @PathVariable을 사용하여 view에서 보낸 파라미터를 받음
    - getUserById(): 파라미터로 받은 유저 id와 저장된 유저 리스트를 대조하여
    해당 유저 객체를 반환해줌

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -4,6 +4,7 @@ import com.codessquad.qna.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -24,6 +25,21 @@ public class UserController {
     public String getUserList(Model model) {
         model.addAttribute("users", userList);
         return "list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String getUserProfile(@PathVariable String userId, Model model) {
+        model.addAttribute("user", getUserById(userId));
+        return "profile";
+    }
+
+    public User getUserById(String userId) {
+        for (User user : userList) {
+            if(userId.equals(user.getUserId())){
+                return user;
+            }
+        }
+        return null;
     }
 
 }

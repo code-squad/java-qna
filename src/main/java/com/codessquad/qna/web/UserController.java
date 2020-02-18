f20b01a601e28c9713c3543d0ca5941290180752
Byung Hyun Choi <choi8608@gmail.com>
2020-02-18 14:33:06 +0900

    feat : Complete step1 '회원 프로필 정보보기'
    - resources/templates/user/list.html
    userId 에 a 태그 추가
    
    - templates/user/list.html
    model 을 통해 전달받은 회원정보를 모두 보여주도록 설정
    
    - templates/user/profile.html
    profile.html 추가 및 email, name 추가
    
    - UserController
    /users/{userId} 추가

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -3,7 +3,9 @@ package com.codessquad.qna.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,16 +15,20 @@ public class UserController {
 
   private static List<User> userList = new ArrayList<>();
 
-  @GetMapping("/form")
-  public String goForm() {
-    return "user/form";
-  }
-
   @PostMapping("/users")
   public String createUser(User user, Model model) {
     userList.add(user);
-
     return "redirect:/list";
+  }
+
+  @RequestMapping("/users/{userId}")
+  public String page(@PathVariable String userId, Model model) {
+    for (User user : userList) {
+      if (userId.equals(user.getUserId()))
+        model.addAttribute("user", user);
+    }
+
+    return "user/profile";
   }
 
   @GetMapping("/list")

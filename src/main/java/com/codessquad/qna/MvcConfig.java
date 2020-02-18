aacfaf29481a7a077c1067070714d5809d4b89c5
beemiel <qnswwrn@gmail.com>
2020-02-18 22:45:00 +0900

    MvcConfig <생성>: 매핑을 쉽게 해주는 MvcConfig 생성
    MvcConfig를 사용하여 정적인 페이지들은 따로 컨트롤러를 생성하지 않고
    바로 매핑을 해줌

---
+++ src/main/java/com/codessquad/qna/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.codessquad.qna;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registry.addViewController("/login").setViewName("../static/user/login");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/user/form").setViewName("form");
+    }
+}

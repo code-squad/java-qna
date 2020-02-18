371a3226854a10ae06e05f37f40e4f9e5d5daa84
Jay <beginin15@gmail.com>
2020-02-19 00:17:39 +0900

    feat : 회원프로필 요청(GET 요청)을 처리할 메소드를 추가하고 매
    * 회원 리스트에서 아이디 클릭 시, 해당 회원의 정보를 보여주는 화면으로 이동
    
    * 클릭이 발생한 회원의 아이디를 URL에 함께 전달
    
    * @PathVariable 을 이용하여 URL에 전달된 아이디 값을 인자로 전달받음
    
    * findUser() : 아이디에 해당되는 회원이 존재하는지 리스트에서 검색하는 메소드
    
    * 아이디에 해당되는 User 객체를 뷰에 출력하기 위해 모델에 저장

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -3,6 +3,7 @@ package com.codessquad.qna;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -23,5 +24,20 @@ public class UserController {
     public String listsOfUsers(Model model) {
         model.addAttribute("users", users);
         return "user/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String profile(@PathVariable("userId") String userId, Model model) {
+        model.addAttribute("user", findUser(userId));
+        return "user/profile";
+    }
+
+    private User findUser(String userId) {
+        for (User user : users) {
+            if(userId.equals(user.getUserId())) {
+                return user;
+            }
+        }
+        return null;
     }
 }

4d5c60c37a6c03a5f918838a1ee6dad2aeeab3f8
dd <>
2020-02-18 16:40:47 +0900

    modify route /list to /users
    요구사항에 맞춰 /list를 /users로 변경했습니다.

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -16,10 +16,10 @@ public class UserController {
     public String create(User user) {
         System.out.println("user => " + user);
         users.add(user);
-        return "redirect:/list";
+        return "redirect:/users";
     }
 
-    @GetMapping("/list")
+    @GetMapping("/users")
     public String list(Model model) {
         model.addAttribute("users", users);
         return "list";

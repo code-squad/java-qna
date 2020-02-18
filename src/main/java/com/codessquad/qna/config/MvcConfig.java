e5b0113fc4435b3b3b2c6bfda8878bc45a90783d
Jay <beginin15@gmail.com>
2020-02-19 03:03:24 +0900

    refactor : setOrder 메소드 제거
    * 무슨 기능인지 몰라서 제거함

--- src/main/java/com/codessquad/qna/config/MvcConfig.java
+++ src/main/java/com/codessquad/qna/config/MvcConfig.java
@@ -1,8 +1,6 @@
 package com.codessquad.qna.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
         registry.addViewController("/user/form").setViewName("user/form");
+        registry.addViewController("/user/login").setViewName("../static/user/login");
         registry.addViewController("/qna/form").setViewName("qna/form");
     }
 }

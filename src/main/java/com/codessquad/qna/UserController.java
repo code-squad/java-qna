089a3aca6941917bfbc4ef4ce495d0743302edce
koo bohyeon <goosee115@gmail.com>
2020-02-18 14:52:47 +0900

    회원 목록 출력 기능
    user/list로 접근 시 list.html에 users를 넘겨주기 위해 메서드를 추가함
    회원 목록을 보여주기 위해서 핸들바 템플릿을 사용하여 동적으로 화면을 만듦

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -1,6 +1,8 @@
 package com.codessquad.qna;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,10 +13,18 @@ import java.util.List;
 public class UserController {
     List<User> users = new ArrayList<>();
 
-    @PostMapping("/users/create")
+    @PostMapping("/user/create")
     public String create(User user) {
         users.add(user);
         System.out.println(users);
-        return "redirect:/users/list";
+        return "redirect:/user/list";
     }
+
+    @GetMapping("/user/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "/user/list";
+    }
+
+
 }

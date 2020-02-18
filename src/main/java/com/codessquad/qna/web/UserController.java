c25d65c2d6175c975e2b3a18f5287e0ffc4d1be8
kyungrae <rlarudfo93@gmail.com>
2020-02-18 12:43:43 +0900

    feat: 회원목록 기능 구현
    css, js 상대경로에서 절대경로로 변경했다.
    
    사용자 리스트 a tag 링크 수정

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -1,6 +1,8 @@
 package com.codessquad.qna.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -9,6 +11,12 @@ import java.util.List;
 @Controller
 public class UserController {
     private List<User> users = new ArrayList<>();
+
+    @GetMapping("/users")
+    public String userList(Model model) {
+        model.addAttribute("users", users);
+        return "/user/list";
+    }
 
     @PostMapping("/users")
     public String createUser(User user) {

c45e02ae9ac9537301af5d7b57d8290b7da5de2d
han95210 <han95210@naver.com>
2020-02-18 10:46:59 +0900

    [환경설정] live reload 구현 완료
    * 매번 서버를 켰다 껐다하기 귀찮아서 구현

--- src/main/java/com/codessquad/qna/QnaApplication.java
+++ src/main/java/com/codessquad/qna/QnaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QnaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(QnaApplication.class, args);
 	}
 

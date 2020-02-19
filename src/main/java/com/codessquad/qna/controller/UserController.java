bbd8d8a19a361378031b719537f2c9f9f6645f45
moonelysian <dandyblack@naver.com>
2020-02-19 13:31:01 +0900

    user controller 수정 - 요구사항 맞춰 path 수정

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -3,33 +3,44 @@ package com.codessquad.qna.controller;
 import com.codessquad.qna.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     private List<User> users = new ArrayList<>();
 
-    @PostMapping("/users")
-    public String createUser(User user) {
-        users.add(user);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/users")
-    public String getUserList(Model model) {
+    @GetMapping
+    public String showUserList(Model model) {
         model.addAttribute("users", users);
         return "user/list";
     }
 
-    @GetMapping("/users/{userIndex}")
-    public String getUserProfile(@PathVariable int userIndex, Model model) {
-       User user = users.get(userIndex);
-       model.addAttribute("user", user);
-       return "user/profile";
+    @GetMapping("/{userId}")
+    public String getUserProfile(@PathVariable String userId, Model model) {
+        for (User user: users) {
+            if(user.getUserId().equals(userId)) {
+                model.addAttribute("user", user);
+                return "user/profile";
+            }
+        }
+        return "/";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String updateUser(@PathVariable int id, Model model) {
+        User user = users.get(id);
+        model.addAttribute("user", user);
+        model.addAttribute("id", id);
+        return "user/edit";
+    }
+
+    @PostMapping
+    public String createUser(User user) {
+        users.add(user);
+        return "redirect:/users";
     }
 }

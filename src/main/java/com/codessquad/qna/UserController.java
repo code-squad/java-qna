d0211fc78a2f3900f4198f87938a414ccb83924a
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-02-19 11:51:35 +0900

    make: showUserList() method in UserController
    get요청을 처리하는 showUserList() method 생성 후 @GetMapping 애노테이션 추가

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -1,6 +1,8 @@
 package com.codessquad.qna;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -14,5 +16,11 @@ public class UserController {
     public String createUser(User user) {
         users.add(user);
         return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String showUserList(Model model) {
+        model.addAttribute("users", users);
+        return "user/list";
     }
 }

d641fd9e02ab7732773fd10bc6b704a77e6a8576
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-02-19 15:28:17 +0900

    make : qna/list.html
    질문목록을 띄어주기 위해 index.html을 qna/list.html에 복사

--- src/main/java/com/codessquad/qna/QuestionController.java
+++ src/main/java/com/codessquad/qna/QuestionController.java
@@ -1,14 +1,26 @@
 package com.codessquad.qna;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class QuestionController {
+    List<Question> questions = new ArrayList<>();
 
     @PostMapping("/questions")
-    public String createQuestion() {
-
+    public String createQuestion(Question question) {
+        questions.add(question);
         return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String showQuestion(Model model) {
+        model.addAttribute("questions", questions);
+        return "qna/list";
     }
 }

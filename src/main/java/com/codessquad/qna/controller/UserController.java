e610e875a92b20f9ca2a0fe82767ca9fa20d66bf
KIM DONG HWAN <quaint3@gmail.com>
2020-02-19 15:35:09 +0900

    Feat : 사용자 추가, 목록 조회, 프로필 조회
    Entity annotation을 이용하여, User class를 DB와 Mapping

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.codessquad.qna.controller;
 
 import com.codessquad.qna.domain.User;
+import com.codessquad.qna.repository.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,28 +17,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
 
-  private static List<User> users = new ArrayList<>();
   private static Logger log = LoggerFactory.getLogger(QnaController.class);
+  private static List<User> users = new ArrayList<>();
+
+  @Autowired
+  private UserRepository userRepository;
 
   @PostMapping(value = "/user/create")
   public String createUser(@ModelAttribute User user) {
-    users.add(user);
+    userRepository.save(user);
     return "redirect:/users";
   }
 
   @GetMapping(value = "/users")
   public String getUsers(Model model) {
-    model.addAttribute("users", users);
+    model.addAttribute("users", userRepository.findAll());
     return "user/list";
   }
 
-  @GetMapping(value = "/users/{userId}")
-  public String getProfile(Model model, @PathVariable("userId") String userId) {
-    User user = users.stream()
-        .filter(u -> userId.equals(u.getUserId()))
-        .findAny()
-        .orElse(null);
-    model.addAttribute("user", user);
+  @GetMapping(value = "/users/{id}")
+  public String getProfile(Model model, @PathVariable("id") String id) {
+    model.addAttribute("user", userRepository.findById(convertToString(id)));
     return "user/profile";
+  }
+
+  private Long convertToString(String id) {
+    return Long.parseLong(id);
   }
 }
--- src/main/java/com/codessquad/qna/domain/User.java
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -1,29 +1,59 @@
 package com.codessquad.qna.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
-  private String userId;
-  private String password;
-  private String name;
-  private String email;
+  @Id
+  @GeneratedValue
+  private Long id;
 
-  public User(String userId, String password, String name, String email) {
-    this.userId = userId;
-    this.password = password;
-    this.name = name;
-    this.email = email;
-  }
+  @Column(nullable = false, length = 25)
+  private String userId;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String email;
 
   public String getUserId() {
     return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
   }
 
   public String getName() {
     return name;
   }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
   public String getEmail() {
     return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
   }
 
   @Override

dfeb44e3702259d9d4508270698cee44c1a84d93
beemiel <qnswwrn@gmail.com>
2020-02-20 01:19:46 +0900

    Controller <추가>: 예외처리 부분 추가
    둘 다 리스트에서 인스턴스를 하나 반환하는 메서드에서 예외를 던지도록
    추가`

--- src/main/java/com/codessquad/qna/controller/QuestionController.java
+++ src/main/java/com/codessquad/qna/controller/QuestionController.java
@@ -3,25 +3,25 @@ package com.codessquad.qna.controller;
 import com.codessquad.qna.Question;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 @Controller
 public class QuestionController {
-    private List<Question> questions = new ArrayList<>();
+    private List<Question> questions = Collections.synchronizedList(new ArrayList<>());
 
     @PostMapping("/qna/create")
     public String createQna(Question question) {
-        question.setTime(getTiime());
+        question.setWriteTime(getWriteTime());
         question.setIndex(questions.size()+1);
         questions.add(question);
-        System.out.println(question);
         return "redirect:/";
     }
 
@@ -32,19 +32,23 @@ public class QuestionController {
     }
 
     @GetMapping("/qna/{index}")
-    public String getQuestion(@PathVariable String index, Model model) {
-        model.addAttribute("question",findQuestion(index));
+    public String getQuestion(@PathVariable int index, Model model) throws ArrayIndexOutOfBoundsException{
+        model.addAttribute("question", getQuestionByIndex(index));
         return "qna/show";
     }
 
-    public Question findQuestion(String index) {
-        return questions.get(Integer.parseInt(index)-1);
+    public Date getWriteTime() {
+        return new Date();
     }
 
-    public String getTiime() {
-        SimpleDateFormat format = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
-        Date time = new Date();
-        return format.format(time);
+    public Question getQuestionByIndex(int index) throws ArrayIndexOutOfBoundsException{
+        for (Question question : questions) {
+            if(index==question.getIndex()){
+                return question;
+            }
+        }
+
+        throw new ArrayIndexOutOfBoundsException();
     }
 
 }
--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -6,40 +6,43 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
+@RequestMapping("/user") // 중복되는 prefix, 자원에 대해 명시해준다.
 public class UserController {
-    private List<User> userList = new ArrayList<>();
+    private List<User> userList = Collections.synchronizedList(new ArrayList<>());
 
-    @PostMapping("/user/create")
+    @PostMapping("/create")
     public String createUser(User user) {
         userList.add(user);
         System.out.println(user);
         return "redirect:/user/list";
     }
 
-    @GetMapping("/user/list")
+    @GetMapping("/list")
     public String getUserList(Model model) {
         model.addAttribute("users", userList);
         return "user/list";
     }
 
-    @GetMapping("/users/{userId}")
-    public String getUserProfile(@PathVariable String userId, Model model) {
+    @GetMapping("/{userId}")
+    public String getUserProfile(@PathVariable String userId, Model model) throws NullPointerException {
         model.addAttribute("user", getUserById(userId));
         return "user/profile";
     }
 
-    public User getUserById(String userId) {
+    public User getUserById(String userId) throws NullPointerException{
         for (User user : userList) {
             if(userId.equals(user.getUserId())){
                 return user;
             }
         }
-        return null;
+        throw new NullPointerException("null");
     }
 
 }

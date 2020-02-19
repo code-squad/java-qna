79d6a1066ea7c76eaad4f4bc935c6255f0f2b688
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-02-19 11:37:33 +0900

    make: User Class
    사용자가 전달한 값을 저장 할 User클래스 생성
    getter, setter, toString() 메소드 생성

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -3,12 +3,16 @@ package com.codessquad.qna;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
+    List<User> users = new ArrayList<>();
 
     @PostMapping("/users")
-    public String createUser() {
-
+    public String createUser(User user) {
+        users.add(user);
         return "redirect:/users";
     }
 }

a9b1fe818391be3a78783f43d4167fd99becbec1
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-21 00:55:09 +0900

    [MODIFY] IMPLEMENTING TRIVIAL ERROR HANDLING METHOD TO USER CONTROLLER

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -8,31 +8,34 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
   private List<User> userList = new ArrayList<>();
 
-  @PostMapping("/user/create")
+  @PostMapping("/create")
   public String createUser(User user) {
     userList.add(user);
     return "redirect:/user/list";
   }
 
-  @GetMapping("/user/list")
+  @GetMapping("/list")
   public String getUserList(Model model) {
     model.addAttribute("users", userList);
-    return "list";
+    return "user/list";
   }
 
-  @GetMapping("/users/{userId}")
-  public String showUserProfile(@PathVariable String userId, Model model) {
+  @GetMapping("/{userId}")
+  public String showUserProfile(@PathVariable String userId, Model model) throws NullPointerException {
     for (User user : userList) {
       if (userId.equals(user.getUserId())) {
         model.addAttribute("user", user);
         break;
       }
+      throw new NullPointerException("no user");
     }
-    return "profile";
+    return "user/profile";
   }
 }

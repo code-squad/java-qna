890903d1675607ddc2a6250fa2cfa5d67bd99e3c
guswns1659 <guswns1659@gmail.com>
2020-02-20 12:11:37 +0900

    20.02.20) 회원 프로필 정보 보기 기능 구현
    @Pathvariable 어노테이션을 활용해 URL을 통해 받은 인자 활용.

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -3,6 +3,7 @@ package com.codessquad.qna;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -24,4 +25,14 @@ public class UserController {
         model.addAttribute("users", users);
         return "list";
     }
+
+    @GetMapping("/users/{userId}")
+    public String profile(@PathVariable String userId, Model model) {
+        for (User each : users) {
+            if (each.getUserId().equals(userId)) model.addAttribute("user", each);
+        }
+        return "profile";
+    }
+
+
 }

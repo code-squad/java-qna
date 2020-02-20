2c39ac8b5cf7b1ae4d82d37ae5e97cb58f64385a
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-21 00:14:19 +0900

    [ADD] USERCONTROLLER THAT MAPS USER PROFILE OF SPECIFIC USERS - learned: @PathVariable. By comparing the user id and parameter, add attribute to model.

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -22,5 +23,16 @@ public class UserController {
   public String getUserList(Model model) {
     model.addAttribute("users", userList);
     return "list";
+  }
+
+  @GetMapping("/users/{userId}")
+  public String showUserProfile(@PathVariable String userId, Model model) {
+    for (User user : userList) {
+      if (userId.equals(user.getUserId())) {
+        model.addAttribute("user", user);
+        break;
+      }
+    }
+    return "profile";
   }
 }

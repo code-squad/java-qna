86c6bd96647ebe62cacd89df4a99a5257e295581
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-20 23:21:05 +0900

    [ADD] USERCONTROLLER THAT MAPS USER CREATION/GETTING USER LIST PAGE - learned how @GetMapping and @PostMapping features

---
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.codessquad.qna.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+  private List<ArrayList> userList = new ArrayList<>();
+
+  @PostMapping("/user/create")
+  public String createUser(User user) {
+    userList.add(user);
+    return "redirect:/user/list";
+  }
+
+  @GetMapping("/user/list")
+  public String getUserList(Model model) {
+    model.addAttribute("users", userList);
+    return "list";
+  }
+}

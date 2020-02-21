42175de91d3d1975306bbed36d3c334473f551a8
Jay <beginin15@gmail.com>
2020-02-21 17:47:57 +0900

    style : 사용자 관련 url을 user로 변경. 그로인한 폴더명 변경

--- src/main/java/com/codesquad/qna/config/MvcConfig.java
+++ src/main/java/com/codesquad/qna/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.codessquad.qna.config;
+package com.codesquad.qna.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/user/form").setViewName("user/form");
+        registry.addViewController("/user/form").setViewName("users/form");
         registry.addViewController("/user/login").setViewName("../static/user/login");
         registry.addViewController("/qna/form").setViewName("qna/form");
     }

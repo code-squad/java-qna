673c7e341e10838fd68beea02101f92eb2bf6579
koo bohyeon <goosee115@gmail.com>
2020-02-21 16:04:19 +0900

    User 클래스를 DB 테이블에 매핑
    jpa를 사용하여 자바 객체 설정에 따라 테이블이 자동설정 되도록 함
    user에 id값을 추가하여 id로 접근하도록 수정함
    id 추가한 사항을 view 에도 적용함

--- src/main/java/com/codessquad/qna/User.java
+++ src/main/java/com/codessquad/qna/User.java
@@ -1,10 +1,26 @@
 package com.codessquad.qna;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, length = 20)
     private String userId;
+
     private String name;
     private String email;
     private String password;
+
+    public Long getId() {
+        return id;
+    }
 
     public String getUserId() {
         return userId;
@@ -36,6 +52,12 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void update(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
     }
 
     @Override

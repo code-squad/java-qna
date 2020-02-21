7d22d39122364683fa5977a387c7953934d43560
haveagood@naver.com <haveagood@naver.com>
2020-02-21 17:13:38 +0900

    <feat> : JPA 어노테이션을 활용하여 Entity클래스 설정 및 CURD repository 생성
    - JPA 어노테이션을 활용하여 User 클래스를 Entity 클래스로 설정하였습니다.
    
    - UserRepository를 CRUD Repository를 상속받게 하여 CRUD 기본 기능을 사용할 수 있게 하였습니다.
    
    - User 클래스에 Long 타입의 id 변수를 생성하고, Prime Key를 설정하였습니다.
    
    - User 클래스의 userId는 Column으로 설정하였습니다.
    
    모르는 내용 :
    - JPA 에 대해서 정확하게 어떤 기능을 하는지 확실하게 모르고 있는것 같습니다.
    - DB의 기본 동작에 대해서 정확하게 모르고 있습니다.
    - userRepository.findById(id).get() 이런식으로 값을 가져오면 null값도 가져오는 경우가 생기기 때문에 별도의 처리를 해야한다고 합니다.
      어떤 방식으로 처리를 할 수 있는지에 대해서 공부를 해야할것 같습니다.
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

--- src/main/java/com/codessquad/qna/User.java
+++ src/main/java/com/codessquad/qna/User.java
@@ -1,7 +1,16 @@
 package com.codessquad.qna;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
     private String userId;
+
     private String password;
     private String name;
     private String email;

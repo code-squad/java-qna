0f3940ee6970333a655abe0cffa76db60fddd369
kyungrae <rlarudfo93@gmail.com>
2020-02-22 17:16:10 +0900

    feat: 회원가입 폼 화면 생성
    서버 사이드 렌더링을 위해 static->template 이동

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -1,60 +1,16 @@
 package com.codessquad.qna.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-    private List<User> users = Collections.synchronizedList(new ArrayList<>());
 
-    @GetMapping("/users")
-    public String userList(Model model) {
-        model.addAttribute("users", users);
-        return "/user/list";
-    }
-
-    @PostMapping("/users")
-    public String createUser(User user) {
-        users.add(user);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/users/{userId}/form")
-    public String updateUser(@PathVariable String userId, Model model) {
-        model.addAttribute("userId", userId);
-        return "/user/updateForm";
-    }
-
-    @PostMapping("/users/{userId}/update")
-    public String updateUser(@PathVariable String userId, String password, String name, String email) {
-        for(User user: users) {
-            if(user.getUserId().equals(userId)) {
-                user.setPassword(password);
-                user.setName(name);
-                user.setEmail(email);
-                break;
-            }
-        }
-        return "redirect:/users";
-    }
-
-    @GetMapping("/users/{userId}")
-    public String userProfile(@PathVariable String userId, Model model) {
-        User target = null;
-        for(User user: users) {
-            if(user.getUserId().equals(userId)) {
-                target = user;
-                break;
-            }
-        }
-        model.addAttribute("user", target);
-        return "/user/profile";
+    @GetMapping("/new")
+    public String createForm() {
+        return "user/createForm";
     }
 }

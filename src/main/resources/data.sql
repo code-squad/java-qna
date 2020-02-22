178a9579cf78527918c701bf59dc67a3a27cb228
IdionKim <ksundong@gmail.com>
2020-02-22 22:17:17 +0900

    Add: Test User 데이터

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER (id, user_id, user_password, user_name, user_email) VALUES (null, 'dion', 'dion123', '디온', 'dion@codesquad.kr');
+INSERT INTO USER (id, user_id, user_password, user_name, user_email) VALUES (null, 'codesquad', 'db1004', '코드스쿼드', 'master@codesquad.kr');

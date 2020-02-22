044ed404814d76d08823cde04c34504c648072dc
kyungrae <rlarudfo93@gmail.com>
2020-02-22 17:54:29 +0900

    feat: 회원가입 사용자 목록 기능 구현

---
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -0,0 +1,53 @@
+package com.codessquad.qna.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

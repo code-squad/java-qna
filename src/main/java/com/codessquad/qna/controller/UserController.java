d04b71482347f75a1d623feaca11d378a4c38b6d
moonelysian <dandyblack@naver.com>
2020-02-22 12:09:08 +0900

    데이터베이스의 테이블과 자바 객체를 매핑 - lombok 사용 - edit, 상세보기 시 user 없을 땐 error 페이지로 이동

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.codessquad.qna.controller;
 
-import com.codessquad.qna.User;
+import com.codessquad.qna.repository.User;
+import com.codessquad.qna.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,45 +16,47 @@ import java.util.List;
 @Controller
 @RequestMapping("/users")
 public class UserController {
-    private List<User> users = new ArrayList<>();
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping
     public String showUserList(Model model) {
-        model.addAttribute("users", users);
+        model.addAttribute("users", userRepository.findAll());
         return "user/list";
     }
 
-    @GetMapping("/{userId}")
-    public String getUserProfile(@PathVariable String userId, Model model) {
-        for (User user: users) {
-            if(user.getUserId().equals(userId)) {
-                model.addAttribute("user", user);
-                return "user/profile";
-            }
+    @GetMapping("/{id}")
+    public String getUserProfile(@PathVariable Long id, Model model) {
+        User user = userRepository.getOne(id);
+        if (userRepository.existsById(id)) {
+            model.addAttribute("user", user);
+            return "user/profile";
         }
-        return "/";
+        return "error/user/notFoundUser";
     }
 
-    @GetMapping("/{index}/edit")
-    public String updateUser(@PathVariable int index, Model model) {
-        User user = users.get(index);
-        model.addAttribute("user", user);
-        model.addAttribute("index", index);
-        return "user/edit";
+    @GetMapping("/{id}/edit")
+    public String updateUser(@PathVariable Long id, Model model) {
+        if (userRepository.existsById(id)){
+            model.addAttribute("user", userRepository.getOne(id));
+            return "user/edit";
+        }
+        return "error/user/notFoundUser";
     }
 
     @PostMapping
     public String createUser(User user) {
-        users.add(user);
+        userRepository.save(user);
         return "redirect:/users";
     }
 
-    @PutMapping("/{index}")
-    public void updateUser(@PathVariable int index, User user, String currentPassword, HttpServletResponse response) throws IOException {
-        String userPassword = users.get(index).getPassword();
+    @PutMapping("/{id}")
+    public void updateUser(@PathVariable Long id, User updateUser, String currentPassword, HttpServletResponse response) throws IOException {
+        User user = userRepository.getOne(id);
+        String userPassword = user.getPassword();
         if (userPassword.equals(currentPassword)) {
-            users.remove(index);
-            users.add(user);
+            user.update(updateUser);
+            userRepository.save(user);
             response.sendRedirect("/users");
             return;
         }
--- src/main/java/com/codessquad/qna/repository/User.java
+++ src/main/java/com/codessquad/qna/repository/User.java
@@ -1,40 +1,37 @@
-package com.codessquad.qna;
+package com.codessquad.qna.repository;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    @Setter
     private String userId;
+
+    @Column(nullable = false)
+    @Setter
     private String password;
+
+    @Setter
     private String name;
+
+    @Setter
     private String email;
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
+    public void update(User updateUser) {
+        this.password = updateUser.password;
+        this.name = updateUser.name;
+        this.email = updateUser.email;
     }
 }

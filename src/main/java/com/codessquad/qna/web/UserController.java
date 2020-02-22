61f2a75f58da53e231284ab581c8cb85fba48bc0
kyungrae <rlarudfo93@gmail.com>
2020-02-22 17:54:29 +0900

    feat: 회원가입 사용자 목록 기능 구현

--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -1,16 +1,35 @@
 package com.codessquad.qna.web;
 
+import com.codessquad.qna.domain.User;
+import com.codessquad.qna.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    UserRepository repository;
+
+    @GetMapping("")
+    public String listPage(Model model) {
+        model.addAttribute("users", repository.findAll());
+        return "users/list";
+    }
+
     @GetMapping("/new")
-    public String createForm() {
-        return "user/createForm";
+    public String createFormPage() {
+        return "users/createForm";
+    }
+
+    @PostMapping("")
+    public String createUser(User user) {
+        repository.save(user);
+        return "redirect:/users";
     }
 }

8d33421c8cbb7564277a8594fab19ce64c6a79b1
kyungrae <rlarudfo93@gmail.com>
2020-02-24 19:34:08 +0900

    feat: 질문 도메인 작성
    생성 날짜를 표현하기 위해 localDateTime 객체와 dateTimeFormatter 객체를 생성했다.

---
+++ src/main/java/com/codessquad/qna/domain/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.codessquad.qna.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+}

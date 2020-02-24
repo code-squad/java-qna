5dee3820e852dd348b781426fa0fec9182f8f855
Jenny <huji3156@gmail.com>
2020-02-24 23:58:24 +0900

    중복되는 html 코드를 분리하여 줄임

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -5,28 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/user/create")
+    @PostMapping("")
     public String create(User user) {
-        System.out.println(user);
-
         userRepository.save(user);
-        return "redirect:/user/list";
+        return "redirect:/users";
     }
 
-    @GetMapping("user/list")
+    @GetMapping("")
     public String list(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "/user/list";
     }
+
+
 }

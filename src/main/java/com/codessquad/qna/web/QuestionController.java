e89d3f00a3a418b3e5460de5de720dacc5b95488
kyungrae <rlarudfo93@gmail.com>
2020-02-24 19:37:35 +0900

    feat: 질문 생성 페이지 질문 확인 페이지 구현
    각 도메인의 수정 함수 이름이 form이 들어가도록 수정했다.
    
    QuestionController 객체가 / get 요청을 다루기 위해 MvcConfig.java 파일을 제거하 RequestMapping('/') 어노테이션을 제거했다.

---
+++ src/main/java/com/codessquad/qna/web/QuestionController.java
@@ -0,0 +1,33 @@
+package com.codessquad.qna.web;
+
+import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.domain.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class QuestionController {
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @GetMapping("/")
+    public String listPage(Model model) {
+        model.addAttribute("questions", questionRepository.findAll());
+        return "questions/home";
+    }
+
+    @GetMapping("/questions/new")
+    public String createFormPage() {
+        return "questions/createForm";
+    }
+
+    @PostMapping("/questions")
+    public String createQuestion(Question question) {
+        questionRepository.save(question);
+        return "redirect:/";
+    }
+}

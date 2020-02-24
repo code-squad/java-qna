13c45bfec56de2837c268de748a6e085ee5ae540
beemiel <qnswwrn@gmail.com>
2020-02-25 01:43:51 +0900

    <수정>: ArrayList를 UserRepository로 변경
    기존에 ArrayList로 저장하던 데이터를 DB와 연결하여 UserRepository로 관리
    사용자를 추가하고 사용자를 조회하는 로직 수정

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,48 +1,44 @@
 package com.codessquad.qna.controller;
 
 import com.codessquad.qna.domain.User;
+import com.codessquad.qna.domain.UserRepository;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/users") // 중복되는 prefix, 자원에 대해 명시해준다.
 public class UserController {
-    private List<User> userList = Collections.synchronizedList(new ArrayList<>());
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/form")
+    public String getQuestionFrom() {
+        System.out.println("회원가입 폼");
+        return "users/form";
+    }
 
     @PostMapping("/create")
     public String createUser(User user) {
-        userList.add(user);
-        System.out.println(user);
+        System.out.println("사용자 생성");
+        userRepository.save(user);
         return "redirect:/users/list";
     }
 
     @GetMapping("/list")
     public String getUserList(Model model) {
-        model.addAttribute("users", userList);
+        System.out.println("사용자 리스트");
+        model.addAttribute("users", userRepository.findAll());
         return "users/list";
     }
 
-    @GetMapping("/{userId}")
-    public String getUserProfile(@PathVariable String userId, Model model) throws NullPointerException {
-        model.addAttribute("user", getUserById(userId));
+    @GetMapping("/{id}")
+    public String getUserProfile(@PathVariable Long id, Model model) throws NotFoundException {
+        System.out.println("사용자 프로필");
+        model.addAttribute("user", userRepository.findById(id).orElseThrow(() -> new NotFoundException("존재하지 않는 회원입니다.")));
         return "users/profile";
-    }
-
-    public User getUserById(String userId) throws NullPointerException{
-        for (User user : userList) {
-            if(userId.equals(user.getUserId())){
-                return user;
-            }
-        }
-        throw new NullPointerException("null");
     }
 
 }

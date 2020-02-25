1e3bcbfc8d8b6a9d19f6cfeb9f557026a2269628
blossun <dnsngu@gmail.com>
2020-02-25 21:35:59 +0900

    [Test] 테스트 데이터 추가
    권한 체크에 대한 테스트를 위해 테스트 데이터를 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,3 @@
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL, ADDRESS, PHONE_NUMBER) VALUES ('solar', '1234', '솔라씨', 'solar@test.com', '서울시 강남구', '010-1111-2222');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL, ADDRESS, PHONE_NUMBER) VALUES ('holar', 'qwer', '홀라', 'holar@test.com', '서울시 종로구', '010-1234-1234');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL, ADDRESS, PHONE_NUMBER) VALUES ('pangsu', '1111', '펭수', 'pangsu@test.com', 'EBS 소품실', '010-4242-4242');

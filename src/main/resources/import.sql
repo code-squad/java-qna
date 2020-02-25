7ca9e47bb6d8c60f5541505ac5804c7a94dd6876
han95210 <han95210@naver.com>
2020-02-25 15:56:05 +0900

    [step3 로그인에 따른 메뉴 처리 구현] 로그인 시 메뉴 바 바뀌는 기능 구현 완료
    * handlebars 문법을 html 에 사용
    * session을 받아오는 것을 어떻게 할지 몰라 삽질함... application.properties에 handlebars.expose-session-attribute=true을 추가하니까 해결됨

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
-INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('javajigi', 'test', '재성', 'javajigi@slipp.net')
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (1,'javajigi', 'test', '재성', 'javajigi@slipp.net')
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (2,'han95210', '1234', '한울', 'han95210@naver.com')

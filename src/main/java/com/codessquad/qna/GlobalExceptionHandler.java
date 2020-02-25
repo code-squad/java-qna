c4968de9603b6bd9b3f7f2d73b0e7953e6f79d09
beemiel <qnswwrn@gmail.com>
2020-02-25 23:03:17 +0900

    <수정>: NotFound 예외 처리 추가
    DB를 추가하면서 찾을 수 없는 리소스의 경우는 NotFoundException을
    던지도록
    한 뒤 GlobalExceptionHandler에서 처리하도록 수정

--- src/main/java/com/codessquad/qna/GlobalExceptionHandler.java
+++ src/main/java/com/codessquad/qna/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.codessquad.qna;
 
+import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,27 +10,14 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NullPointerException.class)
+    @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "찾을 수 없음")
-    public ModelAndView nullException(NullPointerException e) {
+    public ModelAndView nullException(NotFoundException e) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("exception", e);
-        modelAndView.setViewName("error");
+        modelAndView.setViewName("/");
 
         return modelAndView;
     }
-
-    @ExceptionHandler(ArrayIndexOutOfBoundsException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "찾을 수 없음")
-    public ModelAndView arrayBoundException(ArrayIndexOutOfBoundsException e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e);
-        modelAndView.setViewName("error");
-
-        return modelAndView;
-    }
-
-
-
 
 }

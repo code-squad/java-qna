2c5b9e2b578bb82e702baaad465759628bf2165b
blossun <dnsngu@gmail.com>
2020-02-25 20:17:13 +0900

    [Feat] 프로젝트 재실행 시, DB에 초기 데이터 insert 자동화
    import.sql 파일에 초기화 작업 쿼리를 작성해줌

--- src/main/java/com/codesquad/qna/domain/User.java
+++ src/main/java/com/codesquad/qna/domain/User.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotEmpty;
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty
---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL, ADDRESS, PHONE_NUMBER) VALUES ('solar', '1234', '솔라씨', 'solar@test.com', '서울시 강남구', '010-1111-2222');

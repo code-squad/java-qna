40ae9fd7db4ed048ccb9c4abfdb38fc1ad2716fe
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-26 21:43:11 +0900

    [ADD] Posts Save feature implemented - Step1) Posts-save mustache to POST to API Controller - Step2) API Controller receives and throws JSON data to Dto Object(@RequestBody) - PostsService - Step3) PostsService save calls toEntity of Dto and sends it to PostsRepository - Step4) PostsRepository returns id after executing Query message.

--- src/main/java/com/codessquad/qna/controller/PostsAPIController.java
+++ src/main/java/com/codessquad/qna/controller/PostsAPIController.java
@@ -1,5 +1,19 @@
 package com.codessquad.qna.controller;
 
-public class PostsAPIController {
+import com.codessquad.qna.service.posts.PostsService;
+import com.codessquad.qna.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@RestController
+public class PostsAPIController {
+  private final PostsService postsService;
+
+  @PostMapping("/api/v1/posts")
+  public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+    return postsService.save(requestDto);
+  }
 }

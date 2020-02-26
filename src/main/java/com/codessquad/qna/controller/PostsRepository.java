126340790c7c5562727def4719242e002fb78817
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-26 21:47:21 +0900

    [ADD] PostsService : Query for findAllDesc()

--- src/main/java/com/codessquad/qna/controller/PostsRepository.java
+++ src/main/java/com/codessquad/qna/controller/PostsRepository.java
@@ -1,7 +1,11 @@
 package com.codessquad.qna.controller;
 
-import com.codessquad.qna.domain.User;
+import com.codessquad.qna.domain.Posts;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PostsRepository extends CrudRepository<User, Long> {
+public interface PostsRepository extends CrudRepository<Posts, Long> {
+  @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+  List<Posts> findAllDesc();
 }

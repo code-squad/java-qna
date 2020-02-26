f48278a8e4521b32b37ea622294837c9926c8360
suhyunsim <cnc1106@naver.com>
2020-02-26 15:52:46 +0900

    회원가입 기능 구현
    UserController에 회원가입 기능을 구현한다. create 메서드로 데이터를 users 리스트에 담고 redirect를 통해 가입한 유저 목록을 보일 수 있게 한다.
    회원가입을 위한 form url을 매핑하고 중복되는 users부분을 RequestMapping으로 처리한다.

--- src/main/java/com/codessquad/qna/user/UserController.java
+++ src/main/java/com/codessquad/qna/user/UserController.java
@@ -1,19 +1,35 @@
 package com.codessquad.qna.user;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     private List<User> users = new ArrayList<>();
 
-    @PostMapping("/users/create")
+    @GetMapping("/form")
+    public String form() {
+        return "user/form";
+    }
+
+    @PostMapping("/create")
     public String create(User user) {
         System.out.println("User: " + user);
         users.add(user);
-        return "index";
+        return "redirect:/users/list";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "user/list";
     }
 }

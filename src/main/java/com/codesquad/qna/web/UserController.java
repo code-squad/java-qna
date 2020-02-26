c228cc488d70c17d7de8388590f35a3728eee5bd
Jay <beginin15@gmail.com>
2020-02-26 16:05:33 +0900

    feat : 로그인에 성공한 경우, 세션을 생성하여 클라이언트 쿠키에 저장하도록 구현
    * login() : 로그인 폼에서 작성된 아이디가 존재하고, 해당 아이디에 대한 비밀번호가 일치하면 현재 세션에 "sessionedUser"라는 이름으로 유저 정보를 저장함.
    
    * logout() : 현재 세션에서 "sessiondUser"라는 이름의 데이터를 제거함.
    
    * findUser() : 리팩토링. 인자 isThrowable 값에 따라 존재하지 않는 아이디인 경우 404 에러 페이지를 보여주거나 호출한 곳을 null을 리턴하도록 변경함.

--- src/main/java/com/codesquad/qna/web/UserController.java
+++ src/main/java/com/codesquad/qna/web/UserController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
@@ -32,29 +35,54 @@ public class UserController {
 
     @GetMapping("/{userId}")
     public String show(@PathVariable String userId, Model model) {
-        model.addAttribute("user", findUser(userId));
+        model.addAttribute("user", findUser(userId, true));
         return "users/profile";
     }
 
     @GetMapping("/{userId}/form")
     public String updateForm(@PathVariable String userId, Model model) {
-        model.addAttribute("user", findUser(userId));
+        model.addAttribute("user", findUser(userId, true));
         return "users/updateForm";
     }
 
     @PutMapping("/{userId}/update")
     public String update(@PathVariable String userId, User updateUser) {
-        User originUser = findUser(userId);
-        if (!originUser.matchPassword(updateUser)) {
+        User originUser = findUser(userId, true);
+        if (!originUser.matchPassword(updateUser))
             return "users/update_failed";
-        }
+
         originUser.update(updateUser);
         userRepository.save(originUser);
         return "redirect:/users";
     }
 
-    private User findUser(String userId) {
+    @PostMapping("/login")
+    public String login(User sessionUser, HttpSession session) {
+        User user = findUser(sessionUser.getUserId(), false);
+        String loginFailedTemplate = "/users/login_failed";
+        if (user == null)
+            return loginFailedTemplate;
+
+        if (!user.matchPassword(sessionUser))
+            return loginFailedTemplate;
+
+        session.setAttribute("sessionedUser", user);
+        log.info("login Success : {}" , user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("sessionedUser");
+        log.info("logout Success");
+        return "redirect:/";
+    }
+
+    private User findUser(String userId, boolean isThrowable) {
         log.info("findUser: {}", userId);
-        return userRepository.findById(userId).orElseThrow(IllegalArgumentException::new);
+        Optional<User> user = userRepository.findById(userId);
+        if (isThrowable)
+            return user.orElseThrow(IllegalArgumentException::new);
+        return user.orElse(null);
     }
 }

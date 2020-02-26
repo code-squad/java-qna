36def740941129be9ea46ced2e3c5b77e051efa7
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-26 21:51:50 +0900

    [ADD] UserAPIController : process related API requests regarding users - @RequestBody to receive HTML(JSON) object from the request. - render UsersRegisterRequestDto for passing it to usersService.

--- src/main/java/com/codessquad/qna/controller/UsersAPIController.java
+++ src/main/java/com/codessquad/qna/controller/UsersAPIController.java
@@ -1,5 +1,20 @@
 package com.codessquad.qna.controller;
 
+import com.codessquad.qna.service.users.UsersService;
+import com.codessquad.qna.web.dto.UsersRegisterRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class UsersAPIController {
 
+  private final UsersService usersService;
+
+  @PostMapping("/api/v1/users")
+  public Long register(@RequestBody UsersRegisterRequestDto requestDto) {
+    return usersService.register(requestDto);
+  }
 }

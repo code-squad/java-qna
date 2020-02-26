956aca7359defdd00261ca42ca5db3a60089949d
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-26 21:50:58 +0900

    [MODIFY] IndexControllerTest : assertThat import class reference changed

--- src/test/java/com/codessquad/qna/web/IndexControllerTest.java
+++ src/test/java/com/codessquad/qna/web/IndexControllerTest.java
@@ -1,5 +1,6 @@
 package com.codessquad.qna.web;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 import com.codessquad.qna.controller.PostsRepository;
@@ -10,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)

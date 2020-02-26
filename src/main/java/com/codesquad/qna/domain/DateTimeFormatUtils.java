15cf3a197ad526561a6e82f0030eb3bf91c4038f
Jay <beginin15@gmail.com>
2020-02-27 05:49:46 +0900

    refactor : LocalDateTime 을 원하는 문자열로 포맷팅하기 위한 Utils 클래스 정의
    * localDateTimeToString() : Question과 Answer 클래스도 필요한 기능이므로 유틸클래스의 메소드로 정의함.

---
+++ src/main/java/com/codesquad/qna/domain/DateTimeFormatUtils.java
@@ -0,0 +1,10 @@
+package com.codesquad.qna.domain;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatUtils {
+    public static String localDateTimeToString(LocalDateTime localDateTime) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
+}

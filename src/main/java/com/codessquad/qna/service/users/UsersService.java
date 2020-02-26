7e6c674cc2a2c3100cca75f4fb879b989983b4c6
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-26 21:55:00 +0900

    [ADD] UsersService : register method - extract fields from received Dto objects, and throw it to usersRepository after executing Dtos to Entity. - received HTTP Respond Long message.

--- src/main/java/com/codessquad/qna/service/users/UsersService.java
+++ src/main/java/com/codessquad/qna/service/users/UsersService.java
@@ -1,5 +1,18 @@
-package com.codessquad.qna.service.posts;
+package com.codessquad.qna.service.users;
 
+import com.codessquad.qna.controller.UsersRepository;
+import com.codessquad.qna.web.dto.UsersRegisterRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class UsersService {
+  private final UsersRepository usersRepository;
 
+  @Transactional
+  public Long register(UsersRegisterRequestDto requestDto) {
+    return usersRepository.save(requestDto.toEntity()).getId();
+  }
 }

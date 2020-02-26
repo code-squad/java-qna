15299ca3074ed39d36d162fe1984c0fa5a2e311f
David Chung <david215@berkeley.edu>
2020-02-26 16:46:50 +0900

    Use partial templates to replace static pages
    - Replace static pages with dynamic ones with partial templates to reduce
      redundancy.
    - WebMvcConfig.java
        - Implement WebMvcConfigurer.
        - Add view controller for URLs without mapping.

---
+++ src/main/java/io/david215/qna/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package io.david215.qna;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/qna/form").setViewName("qna/form");
+        registry.addViewController("/qna/show").setViewName("qna/show");
+        registry.addViewController("/users/form").setViewName("users/form");
+        registry.addViewController("/users/login").setViewName("users/login");
+        registry.addViewController("/users/login-failed").setViewName("users/login-failed");
+    }
+}

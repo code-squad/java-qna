b0dfa2b518415b6ae8180510cefcd2d86d136226
Jay <beginin15@gmail.com>
2020-02-27 03:56:11 +0900

    feat : 질문 수정 및 삭제 기능 구현
    * createForm() : 로그인된 사용자만 질문을 작성할 수 있도록 구현
    
    * create() : 인자로 Question 객체를 전달받는 것이 아니라, title, contents를 따로 전달받아서 직접 Question 객체를 new로 생성하도록 구현. 현재 세션에 저장된 로그인 회원을 작성글의 주인 즉, 외래키로 맵핑하기 위해 생성자 인자로 전달
    
    * findQuestion() : 인자로 전달된 questionId에 해당되는 객체를 Question 테이블에서 검색하는 로직을 따로 뺀 메소드
    
    * updateForm () : questionId에 해당되는 질문 객체의 작성자와 현재 세션의 로그인된 사용자 정보를 비교하여 현재 로그인된 회원의 작성글이라면 질문 수정 폼으로 이동하도록 구현
    
    * update() : 현재 로그인된 회원의 작성글을 수정하는 것이라면 Question 테이블의 정보를 update하도록 구현
    
    * delete() : update와 마찬가지로 현재 로그인된 회원의 작성글을 삭제하는 것이라면 Question 테이블에서 해당 작성글을 삭제하도록 구현

--- src/main/java/com/codesquad/qna/web/QuestionController.java
+++ src/main/java/com/codesquad/qna/web/QuestionController.java
@@ -2,15 +2,13 @@ package com.codesquad.qna.web;
 
 import com.codesquad.qna.domain.Question;
 import com.codesquad.qna.domain.QuestionRepository;
+import com.codesquad.qna.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
@@ -23,9 +21,21 @@ public class QuestionController {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @GetMapping("/form")
+    public String createForm(HttpSession session) {
+        if (!HttpSessionUtils.isLogined(session))
+            return "redirect:/user/login";
+
+        return "qna/form";
+    }
+
     @PostMapping("")
-    public String create(Question newQuestion, HttpSession session) {
-        newQuestion.setCreatedDateTime(LocalDateTime.now());
+    public String create(String title, String contents, HttpSession session) {
+        if (!HttpSessionUtils.isLogined(session))
+            return "redirect:/user/login";
+
+        User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+        Question newQuestion = new Question(sessionedUser, title, contents, LocalDateTime.now());
         questionRepository.save(newQuestion);
         log.info("create : {}", newQuestion);
         return "redirect:/";
@@ -33,8 +43,52 @@ public class QuestionController {
 
     @GetMapping("/{questionId}")
     public String show(@PathVariable Long questionId, Model model) {
-        Question focusQuestion = questionRepository.findById(questionId).orElseThrow(IllegalArgumentException::new);
+        Question focusQuestion = findQuestion(questionId);
         model.addAttribute("question", focusQuestion);
         return "qna/show";
+    }
+
+    @GetMapping("/{questionId}/form")
+    public String updateForm(@PathVariable Long questionId, HttpSession session, Model model) {
+        if (!HttpSessionUtils.isLogined(session))
+            return "redirect:/user/login";
+
+        Question updateQuestion = getMatchedQuestion(questionId, session);
+        model.addAttribute("question", updateQuestion);
+        return "qna/updateForm";
+    }
+
+    @PutMapping("/{questionId}/update")
+    public String update(@PathVariable Long questionId, String title, String contents, HttpSession session) {
+        if (!HttpSessionUtils.isLogined(session))
+            return "redirect:/user/login";
+
+        Question updateQuestion = getMatchedQuestion(questionId, session);
+        updateQuestion.update(title, contents);
+        questionRepository.save(updateQuestion);
+        return "redirect:/questions/" + questionId;
+    }
+
+    @DeleteMapping("/{questionId}/delete")
+    public String delete(@PathVariable Long questionId, HttpSession session) {
+        if (!HttpSessionUtils.isLogined(session))
+            return "redirect:/user/login";
+
+        Question deleteQuestion = getMatchedQuestion(questionId, session);
+        questionRepository.delete(deleteQuestion);
+        return "redirect:/";
+    }
+
+    private Question findQuestion(Long questionId) {
+        return questionRepository.findById(questionId).orElseThrow(IllegalArgumentException::new);
+    }
+
+    private Question getMatchedQuestion(Long questionId, HttpSession session) {
+        User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+        Question matchedQuestion = findQuestion(questionId);
+        if (!matchedQuestion.matchWriter(sessionedUser))
+            throw new IllegalStateException("You can only update your own");
+
+        return matchedQuestion;
     }
 }

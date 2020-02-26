8ba306155daa273de9f90b4dda5ea2104ab491eb
koo bohyeon <goosee115@gmail.com>
2020-02-26 18:51:55 +0900

    테스트 데이터 추가
    user 테스트용 데이터 추가함

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER(id, user_id, password, name, email) VALUES(1, '엘리', '1234', '아무개', 'ellie@gmail.com');
+INSERT INTO USER(id, user_id, password, name, email) VALUES(2, 'amuge', '4321', '김아무개', 'amuge@gmail.com');

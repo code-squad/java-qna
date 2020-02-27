d7bf7abb4cd85b2a499065e03d09ca3a83653bf6
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-27 23:28:08 +0900

    [ADD] PostsService : update method - the method features updating the info of the post. - 1) finding the object that is going to be updated with utilizing findById method.   2) if not able to find any Id, throw error.   3) calls posts update method with providing title, content through the getter of PostsUpdateRequestDto.

--- src/main/java/com/codessquad/qna/service/posts/PostsService.java
+++ src/main/java/com/codessquad/qna/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package com.codessquad.qna.service.posts;
 
 import com.codessquad.qna.controller.PostsRepository;
+import com.codessquad.qna.domain.Posts;
 import com.codessquad.qna.web.dto.PostsListResponseDto;
+import com.codessquad.qna.web.dto.PostsResponseDto;
 import com.codessquad.qna.web.dto.PostsSaveRequestDto;
+import com.codessquad.qna.web.dto.PostsUpdateRequestDto;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +21,28 @@ public class PostsService {
   @Transactional(readOnly = true)
   public List<PostsListResponseDto> findAllDesc() {
     return postsRepository.findAllDesc().stream()
-        .map(PostsListResponseDto::new)
+        .map(posts -> new PostsListResponseDto(posts))
         .collect(Collectors.toList());
   }
 
   @Transactional
   public Long save(PostsSaveRequestDto requestDto) {
     return postsRepository.save(requestDto.toEntity()).getId();
+  }
+
+  @Transactional
+  public Long update(Long id, PostsUpdateRequestDto requestDto) {
+    Posts posts = postsRepository.findById(id).orElseThrow(
+        () -> new IllegalArgumentException("no such post." + " id = " + id));
+    posts.update(requestDto.getTitle(), requestDto.getContent());
+    return id;
+  }
+
+  @Transactional(readOnly = true)
+  public PostsResponseDto findById(Long id) {
+    Posts entity = postsRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("no such post" + " id = " + id)
+        );
+    return new PostsResponseDto(entity);
   }
 }

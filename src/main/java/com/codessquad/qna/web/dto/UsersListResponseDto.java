002a02726cee88992e985b3a05e54581efd51f8e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-27 14:05:15 +0900

    [ADD] UsersListResponseDto

--- src/main/java/com/codessquad/qna/web/dto/UsersListResponseDto.java
+++ src/main/java/com/codessquad/qna/web/dto/UsersListResponseDto.java
@@ -1,5 +1,20 @@
 package com.codessquad.qna.web.dto;
 
-public class UsersListResponseDto {
+import com.codessquad.qna.domain.Users;
+import java.time.LocalDateTime;
 
+public class UsersListResponseDto {
+  private Long Id;
+  private String UserId;
+  private String password;
+  private String name;
+  private String email;
+
+  public UsersListResponseDto(Users entity) {
+    this.Id = entity.getId();
+    this.UserId = entity.getUserId();
+    this.password = entity.getPassword();
+    this.name = entity.getName();
+    this.email = entity.getEmail();
+  }
 }

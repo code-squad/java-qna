fe2b93b8b30a2f4f3d383930dae682d3663d5b23
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-27 23:32:14 +0900

    [MODIFY] PostsRepository : variable change. id to Id

--- src/main/java/com/codessquad/qna/controller/PostsRepository.java
+++ src/main/java/com/codessquad/qna/controller/PostsRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostsRepository extends CrudRepository<Posts, Long> {
-  @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+  @Query("SELECT p FROM Posts p ORDER BY p.Id DESC")
   List<Posts> findAllDesc();
 }

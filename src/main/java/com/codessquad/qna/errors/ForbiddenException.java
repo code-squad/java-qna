4742770e38fc867b3ad197db20c4dd44a96ea8d2
Byung Hyun Choi <choi8608@gmail.com>
2020-02-27 20:01:11 +0900

    refactor : 코드 정리

--- src/main/java/com/codessquad/qna/errors/ForbiddenException.java
+++ src/main/java/com/codessquad/qna/errors/ForbiddenException.java
@@ -3,6 +3,8 @@ package com.codessquad.qna.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "잘못된 접근입니다.")
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
 public class ForbiddenException extends RuntimeException {
 }
+
+

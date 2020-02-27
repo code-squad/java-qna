14d0c5333eb638c9b32ecb0a6715d4dffb29efba
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-27 23:15:30 +0900

    [ADD] UsersUpdateRequestDto : processes data transfer for user update

--- src/main/java/com/codessquad/qna/web/dto/UsersUpdateRequestDto.java
+++ src/main/java/com/codessquad/qna/web/dto/UsersUpdateRequestDto.java
@@ -1,5 +1,21 @@
 package com.codessquad.qna.web.dto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class UsersUpdateRequestDto {
+  private String userId;
+  private String password;
+  private String name;
+  private String email;
 
+  @Builder
+  public UsersUpdateRequestDto(String userId, String password, String name, String email) {
+    this.userId = userId;
+    this.password = password;
+    this.name = name;
+    this.email = email;
+  }
 }

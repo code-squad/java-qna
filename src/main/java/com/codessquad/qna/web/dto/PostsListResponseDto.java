02baec6c275836e1913b4de33ce0eeadd986831e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-27 23:32:45 +0900

    [ADD] PostsListResponseDto : work in the process of getting the list of posts

--- src/main/java/com/codessquad/qna/web/dto/PostsListResponseDto.java
+++ src/main/java/com/codessquad/qna/web/dto/PostsListResponseDto.java
@@ -2,17 +2,19 @@ package com.codessquad.qna.web.dto;
 
 import com.codessquad.qna.domain.Posts;
 import java.time.LocalDateTime;
+import lombok.Getter;
 
+@Getter
 public class PostsListResponseDto {
+  private Long Id;
   private String title;
-  private String content;
   private String author;
-  private LocalDateTime createdTime;
+  private LocalDateTime modifiedDate;
 
   public PostsListResponseDto(Posts entity) {
+    this.Id = entity.getId();
     this.title = entity.getTitle();
-    this.content = entity.getContent();
     this.author = entity.getAuthor();
-    this.createdTime = entity.getCreatedTime();
+    this.modifiedDate= entity.getModifiedDate();
   }
 }

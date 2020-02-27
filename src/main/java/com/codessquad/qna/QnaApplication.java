3357626cc3a397bc943d4cef1babf7f8bbb32cc7
Byung Hyun Choi <choi8608@gmail.com>
2020-02-27 20:03:39 +0900

    feat : lombok + Slf4j 설정
    lombok + Slf4j 설정

--- src/main/java/com/codessquad/qna/QnaApplication.java
+++ src/main/java/com/codessquad/qna/QnaApplication.java
@@ -1,13 +1,15 @@
 package com.codessquad.qna;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class QnaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QnaApplication.class, args);
+		log.info("Simple log statement with inputs {}, {} and {}", 1, 2, 3);
 	}
-
 }
--- src/main/java/com/codessquad/qna/commons/CommonController.java
+++ src/main/java/com/codessquad/qna/commons/CommonController.java
@@ -6,13 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @Controller
 public class CommonController {
-
-  private static final Logger log = LoggerFactory.getLogger(CommonController.class);
 
   @Autowired
   private QuestionRepository questionRepository;
@@ -22,9 +17,6 @@ public class CommonController {
    */
   @GetMapping(value = {"", "/welcome"})
   public String welcome(Model model) {
-    log.debug("### welcome debug log message");
-    log.info("### welcome info log message");
-
     model.addAttribute("questions", questionRepository.findAll());
 
     return "/welcome";

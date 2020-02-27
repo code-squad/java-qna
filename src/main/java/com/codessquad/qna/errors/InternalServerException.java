f211feaea84c6aa7127a74dbc8c6328cfac14db7
Byung Hyun Choi <choi8608@gmail.com>
2020-02-27 20:16:04 +0900

    feat : login 구현 1
    - 동적인 login.html 작성
    - session 저장에 대한 메소드 구현

---
+++ src/main/java/com/codessquad/qna/errors/InternalServerException.java
@@ -0,0 +1,8 @@
+package com.codessquad.qna.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServerException extends RuntimeException {
+}

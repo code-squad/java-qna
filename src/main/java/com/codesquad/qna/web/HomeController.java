9d0081c0235f091ae0af683cca67e3225daa4950
Jay <beginin15@gmail.com>
2020-02-27 20:37:12 +0900

    style : 아직 구현하지 못한 URL 제거 및 불필요한 내용들 제거

--- src/main/java/com/codesquad/qna/web/HomeController.java
+++ src/main/java/com/codesquad/qna/web/HomeController.java
@@ -1,6 +1,7 @@
 package com.codesquad.qna.web;
 
-import com.codesquad.qna.domain.QuestionRepository;
+import com.codesquad.qna.model.QuestionRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
     @Autowired
     private QuestionRepository questionRepository;
 

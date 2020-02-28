ea1ab505b4fe9522bac35117a714dd2b1b564a3a
suhyunsim <cnc1106@naver.com>
2020-02-29 02:42:15 +0900

    질문 목록 구현 후 QuestionController에서 PostMapping
    질문 폼에서 작성한 질문을 메인 화면에 목록으로 PostMapping 해서 나타낸다.

--- src/main/java/com/codessquad/qna/QuestionController.java
+++ src/main/java/com/codessquad/qna/QuestionController.java
@@ -1,8 +1,11 @@
 package com.codessquad.qna;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +16,21 @@ public class QuestionController {
     @GetMapping("/questions/form")
     public String form() {
         return "/qna/form";
+    }
+
+    @PostMapping("/questions")
+    public String create(Question newQuestion) {
+        newQuestion.setDateTime(LocalDateTime.now());
+        newQuestion.setQuestionIndex(questions.size() + 1);
+        questions.add(newQuestion);
+        System.out.println(newQuestion);
+        return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("questions", questions);
+        return "/index";
     }
 
 }

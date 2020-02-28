6c34d00653232739fb0b17733e4697a35f1b62bd
KIM DONG HWAN <quaint3@gmail.com>
2020-02-28 14:05:41 +0900

    Fix : User Update 시 putMapping 되도록 수정

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -43,7 +44,7 @@ public class UserController {
     return "redirect:/questions/";
   }
 
-  @PostMapping(value = "/{id}")
+  @PutMapping(value = "/{id}")
   public String update(@PathVariable("id") long id, User updateUser,
       HttpSession httpSession) {
     if (!HttpSessionUtils.isLoginUser(httpSession)) {

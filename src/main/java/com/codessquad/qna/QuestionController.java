b6e4267360b6188d75583c130ab88bdd500107b9
suhyunsim <cnc1106@naver.com>
2020-02-29 02:19:25 +0900

    QuestionController 생성
    QuestionController를 생성하고 질문 관련 데이터를 담을 questions 리스트를 생성한다.

---
+++ src/main/java/com/codessquad/qna/QuestionController.java
@@ -0,0 +1,12 @@
+package com.codessquad.qna;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class QuestionController {
+    private List<Question> questions = new ArrayList<>();
+
+}

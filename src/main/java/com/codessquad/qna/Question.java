4d58ff490ec48938ce201d96a5fafd6e6dc7edc7
koo bohyeon <goosee115@gmail.com>
2020-02-28 15:38:19 +0900

    GeneratedValue stragey 설정함
    IDENTITY로 설정하여 자동으로 1씩 증가하도록 설정함
    자동으로 id가 생성되므로 테스트 데이터 생성 쿼리에 id를 삭제함

--- src/main/java/com/codessquad/qna/Question.java
+++ src/main/java/com/codessquad/qna/Question.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 public class Question {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO USER(id, user_id, password, name, email) VALUES(1, '엘리', '1234', '아무개', 'ellie@gmail.com');
-INSERT INTO USER(id, user_id, password, name, email) VALUES(2, 'amuge', '4321', '김아무개', 'amuge@gmail.com');
-
+INSERT INTO USER(user_id, password, name, email) VALUES( '엘리', '1234', '아무개', 'ellie@gmail.com');
+INSERT INTO USER( user_id, password, name, email) VALUES( 'amuge', '4321', '김아무개', 'amuge@gmail.com');
+INSERT INTO QUESTION( contents, created, title, writer_id) VALUES('안녕!', '2020-02-28 02:17:24.448', 'dddd', 2 );
+INSERT INTO ANSWER( contents, created_at, question_id, writer_id) VALUES( 'comments!!', '2020-02-28 02:17:24.444', 1, 2 );

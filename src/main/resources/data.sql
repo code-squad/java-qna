37af255ad25eaf8580957dfca315fc22f3c62d0b
koo bohyeon <goosee115@gmail.com>
2020-02-28 23:09:05 +0900

    테스트 데이터 쿼리 수정함
    created_at으로 필드명이 바뀌어서 데이터 컬럼명도 수정함

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USER(user_id, password, name, email) VALUES( '엘리', '1234', '아무개', 'ellie@gmail.com');
+INSERT INTO USER(user_id, password, name, email) VALUES( 'ellie', '1234', '엘리', 'ellie@gmail.com');
 INSERT INTO USER( user_id, password, name, email) VALUES( 'amuge', '4321', '김아무개', 'amuge@gmail.com');
-INSERT INTO QUESTION( contents, created, title, writer_id) VALUES('안녕!', '2020-02-28 02:17:24.448', 'dddd', 2 );
-INSERT INTO ANSWER( contents, created_at, question_id, writer_id) VALUES( 'comments!!', '2020-02-28 02:17:24.444', 1, 2 );
+INSERT INTO QUESTION( contents, created_at, title, writer_id) VALUES('스프링이 뭔가요', '2020-02-28 02:17:24.444', '스프링', 2 );
+INSERT INTO ANSWER( contents, created_at,  question_id, writer_id) VALUES( '몰라요', '2020-02-28 02:17:24.444', 1, 1 );

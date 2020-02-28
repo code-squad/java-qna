4cd226884d9746157c923431e68a4eab21e42764
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-02-28 19:47:44 +0900

    [ADD][WIP] Users Session Login Test - attempting to test user session login by utilising MockMvc to post appripriate parameters. - however, faced the following error: * Consider defining a bean of type 'com.codessquad.qna.service.posts.PostsService' in your configuration. - since the priority is low for implementing test, this commit is now in "Working in Process"

---
+++ src/test/java/com/codessquad/qna/web/UsersLoginTest.java
@@ -0,0 +1,53 @@
+package com.codessquad.qna.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.codessquad.qna.QnaApplication;
+import com.codessquad.qna.controller.UsersAPIController;
+import com.codessquad.qna.controller.UsersRepository;
+import com.codessquad.qna.service.posts.PostsService;
+import org.h2.engine.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class UsersLoginTest {
+  @Autowired
+  private MockMvc mockMvc;
+  private MockHttpSession httpSession;
+
+  @Autowired
+  private PostsService postsService;
+
+  @Autowired
+  private UsersAPIController usersAPIController;
+
+  @Before
+  public void setUp() throws Exception {
+    mockMvc = MockMvcBuilders.standaloneSetup(usersAPIController).build();
+  }
+
+  @Test
+  public void Users가_로그인된다() throws Exception {
+    String userId = "javajigi";
+    String password = "test";
+
+    mockMvc.perform(post("/login").param("javajigi", "test"))
+        .andExpect(status().isOk())
+        .andReturn();
+    Assert.assertNotNull(httpSession.getSessionContext());
+  }
+}

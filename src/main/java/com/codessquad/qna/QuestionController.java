fb78d560daf34aa9733c1a594e3a2e4cbc05bf97
suhyunsim <cnc1106@naver.com>
2020-02-29 02:26:13 +0900

    QuestionController에 질문 폼 GetMapping
    메인 화면의 질문하기 버튼 클릭 시 질문 폼으로 연결되도록 Get Mapping한다.

--- src/main/java/com/codessquad/qna/QuestionController.java
+++ src/main/java/com/codessquad/qna/QuestionController.java
@@ -1,6 +1,7 @@
 package com.codessquad.qna;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,5 +9,10 @@ import java.util.List;
 @Controller
 public class QuestionController {
     private List<Question> questions = new ArrayList<>();
+
+    @GetMapping("/questions/form")
+    public String form() {
+        return "/qna/form";
+    }
 
 }

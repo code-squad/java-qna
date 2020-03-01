cb29c1f2bf44cbc3b004d3706db17cbcaebfd661
moonelysian <dandyblack@naver.com>
2020-03-01 16:09:00 +0900

    로그아웃 시 alert 후 redirect - 302가 올줄 알았는데 200이 온다 - redirected는 true - redirect 하고픈 url은 어디에..?

--- src/main/resources/static/js/logout.js
+++ src/main/resources/static/js/logout.js
@@ -1,9 +1,10 @@
-const url = "/logout";
+const logoutUrl = "/logout";
 const logout = () => {
-    fetch(url, {
+    fetch(logoutUrl, {
         method: 'POST'
     }).then(response => {
         if (response.redirected && response.ok) {
+            alert("로그아웃 되었습니다")
             window.location.href = "/"
         }
     })

a11648b7fe956c4759720558500e60e1e8f8275c
haveagood@naver.com <haveagood@naver.com>
2020-03-02 12:17:00 +0900

    <refactor> : UserController를 DB를 사용하여 동작하도록 수정
    - 사용자 정보, 사용자 리스트, 사용자 정보 수정등 기존의 ArrayList를 사용헀던 부분을 모두 UserRepository를 사용하도록 수정
    
    - 사용자 정보를 수정할 때, 아이디 비밀번호를 입력하는것이 아니라, 비밀번호만 입력하는 창을 별도로 만들어서 비밀번호만 매칭하도록 수정
    
    # 꼬릿말은 아래에 작성 : ex) #이슈 번호
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

--- src/main/java/com/codessquad/qna/UserController.java
+++ src/main/java/com/codessquad/qna/UserController.java
@@ -32,29 +32,56 @@ public class UserController {
     @GetMapping("/{id}")
     public ModelAndView show(@PathVariable Long id) {
         ModelAndView modelAndView = new ModelAndView("user/profile");
-        modelAndView.addObject("user",userRepository.findById(id).orElse(null));
+        modelAndView.addObject("user", userRepository.findById(id).orElse(null));
         return modelAndView;
     }
 
     @GetMapping("/changeUserInfo/{id}")
     public ModelAndView userLoginForm(@PathVariable Long id) {
-        ModelAndView modelAndView = new ModelAndView("user/updateForm");
-        modelAndView.addObject("user",userRepository.findById(id).orElse(null));
+        ModelAndView modelAndView = new ModelAndView("/user/loginOnlyPassword");
+        User user = userRepository.findById(id).orElse(null);
+        modelAndView.addObject("userId", user.getUserId());
         return modelAndView;
     }
+
+    @PostMapping("/changeUserInfo/{id}")
+    public String userLoginCheck(@PathVariable("id") Long id, String password) {
+        User user = userRepository.findById(id).orElse(null);
+        String userPassword = user.getPassword();
+        if (userPassword.equals(password)) {
+            return "redirect:/users/{id}/update";
+        }
+        return "/user/login_failed";
+    }
+
+    @GetMapping("/{id}/update")
+    public ModelAndView changeUserInfoBeforeLogin(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("user/updateForm");
+        User user = userRepository.findById(id).orElse(null);
+        modelAndView.addObject("userId", user.getUserId());
+        return modelAndView;
+    }
+
     @PostMapping("/{id}/update")
     public String changeUserInfoForm(@PathVariable("id") Long id, String userId, String password, String name, String email) {
         User user = userRepository.findById(id).orElse(null);
-        if(password.length() > 0){
+        if (password.length() > 0) {
             user.setPassword(password);
         }
-        if(name.length() > 0){
+        if (name.length() > 0) {
             user.setName(name);
         }
-        if(email.length() > 0){
+        if (email.length() > 0) {
             user.setEmail(email);
         }
         userRepository.save(user);
         return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/profile")
+    public ModelAndView userProfile(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("user/profile");
+        modelAndView.addObject("user", userRepository.findById(id).orElse(null));
+        return modelAndView;
     }
 }

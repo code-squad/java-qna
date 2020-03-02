aaac548b2562b7b7377b9e47c6a9f8cd7385ed3c
Byung Hyun Choi <choi8608@gmail.com>
2020-03-02 21:15:02 +0900

    refactor : 코드 개선
    - javadoc 개선
    - 형 변환 개선

--- src/main/java/com/codessquad/qna/commons/Utils.java
+++ src/main/java/com/codessquad/qna/commons/Utils.java
@@ -8,10 +8,15 @@ import java.util.Optional;
 
 public class Utils {
 
+  /**
+   * Feat : Null 을 처리한 User 를 가져옵니다.
+   * Desc :
+   * Return : sessionedUser
+   */
   public static User getSessionedUser(HttpSession session) {
-    Optional<User> optionalUser = Optional.ofNullable((User) session.getAttribute("sessionedUser"));
-    User user = optionalUser.orElseThrow(() -> new UserException(CustomErrorCode.USER_NOT_LOGIN));
+    Optional<Object> optionalUser = Optional.ofNullable(session.getAttribute("sessionedUser"));
+    Object user = optionalUser.orElseThrow(() -> new UserException(CustomErrorCode.USER_NOT_LOGIN));
 
-    return user;
+    return (User) user;
   }
 }

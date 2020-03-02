2a7d1e85db4764c14dfe4575e1f0b9661361c8ea
Byung Hyun Choi <choi8608@gmail.com>
2020-03-02 19:32:41 +0900

    refactor : get, post 처리를 위한 메소드 분리

--- src/main/java/com/codessquad/qna/commons/CommonController.java
+++ src/main/java/com/codessquad/qna/commons/CommonController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CommonController {
@@ -15,8 +16,15 @@ public class CommonController {
   /**
    * 모든 Question 을 가져와서 welcome 페이지에 보여줍니다.
    */
-  @GetMapping(value = {"", "/welcome"})
-  public String welcome(Model model) {
+  @GetMapping(value = {"/", ""})
+  public String welcomeGet(Model model) {
+    model.addAttribute("questions", questionRepository.findAll());
+
+    return "/welcome";
+  }
+
+  @PostMapping("/welcome")
+  public String welcomePost(Model model) {
     model.addAttribute("questions", questionRepository.findAll());
 
     return "/welcome";

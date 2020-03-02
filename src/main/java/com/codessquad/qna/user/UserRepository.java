8ffd6a7b3ed5e487caffc80091e98d9e0d87ed39
Byung Hyun Choi <choi8608@gmail.com>
2020-03-02 19:36:58 +0900

    refactor : getUser() method 추가
    User 를 가져오면서 Null 체크를 할 수 있도록 개선

--- src/main/java/com/codessquad/qna/user/UserRepository.java
+++ src/main/java/com/codessquad/qna/user/UserRepository.java
@@ -2,7 +2,9 @@ package com.codessquad.qna.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
-  User findByUserId(String userId);
+  Optional<User> findByUserId(String userId);
 }
 

0ad00fc1914000a3e2d225ab7e69b055f0765bb8
haveagood@naver.com <haveagood@naver.com>
2020-03-03 12:54:54 +0900

    <refactor> : RequestMapping 삭제 및 index 이름 변경
    - 코드리뷰에서 지적받은 RequestMapping 수정 및 index라는 이름을 변경하였습니다.
    - RequestMapping으로 구현했던 부분을 간단하게 POST, GET Mapping으로 수정하였습니다.
    - index를 Mapping하는 부분을 삭제하였습니다.
    - index라는 이름을 home으로 변경하였습니다.
    
    # 꼬릿말은 아래에 작성 : ex) #이슈 번호
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

--- src/main/java/com/codessquad/qna/QuestionController.java
+++ src/main/java/com/codessquad/qna/QuestionController.java
@@ -3,9 +3,7 @@ package com.codessquad.qna;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,24 +11,24 @@ public class QuestionController {
     @Autowired
     private QuestionRepository questionRepository;
 
-    @RequestMapping(value = "/qna/form", method = {RequestMethod.GET})
+    @GetMapping("/qna/form")
     public String createQuestion() {
         return "/qna/form";
     }
 
-    @RequestMapping(value = "/qna/form", method = {RequestMethod.POST})
+    @PostMapping("/qna/form")
     public String makeQuestion(Question question, Model model) {
         questionRepository.save(question);
         return "redirect:/";
     }
 
-    @RequestMapping(value = {"/", "/index"}, method = {RequestMethod.GET})
+    @GetMapping("/")
     public String showQuestionList(Model model) {
         model.addAttribute("questions", questionRepository.findAll());
-        return "/index";
+        return "/home";
     }
 
-    @RequestMapping(value = "/questions/{questionIndex}", method = RequestMethod.GET)
+    @GetMapping("/questions/{questionIndex}")
     public ModelAndView questionShowDetail(@PathVariable Long questionIndex) {
         ModelAndView modelAndView = new ModelAndView("/qna/show");
         modelAndView.addObject("question", questionRepository.getOne(questionIndex));

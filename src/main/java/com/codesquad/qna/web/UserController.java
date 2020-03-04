5d36751420e7dab701cb46982269cf03a63b9e7a
Museop Kim <museopkim0214@gmail.com>
2020-03-04 14:47:06 +0900

    회원 정보 보기 구현
    회원 목록에서 회원 정보를 조회하는 기능을 구현 하였습니다.
    - /users/{userId} 주소에 GetMapping
    - userId를 매개변수로 받아와서 일치하는 유저 정보를 users 리스트에서 검색
    - 검색한 인스턴스를 View(profile.html) 로 전달
    - 전달된 인스턴스의 필드 값을(userName, email) 화면에 출력

--- src/main/java/com/codesquad/qna/web/UserController.java
+++ src/main/java/com/codesquad/qna/web/UserController.java
@@ -3,6 +3,7 @@ package com.codesquad.qna.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -25,5 +26,21 @@ public class UserController {
         model.addAttribute("users", users);
 
         return "list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String printProfile(@PathVariable String userId, Model model) {
+        User selectedUser = null;
+
+        for (User user : users) {
+          if (user.getUserId().equals(userId)) {
+              selectedUser = user;
+              break;
+          }
+        }
+
+        model.addAttribute("user", selectedUser);
+
+        return "profile";
     }
 }

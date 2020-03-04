7e57b455437e3d6a863206b1afea12e67902fe14
KIM DONG HWAN <quaint3@gmail.com>
2020-03-04 18:47:28 +0900

    Chore : countOfAnwer 항목 데이터 삽입

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO USER (id, user_id, password, name, email) VALUES (1, 'master', '1234', '관리자', 'master@gmail.com');
 INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'guest', '1234', '손님', 'guest@gmail.com');
 
-INSERT INTO QUESTION (id, created_time, contents, title, writer_id) VALUES (1, CURRENT_TIMESTAMP, '테스트 질문입니다', 'guest의 테스트 질문', 2);
-INSERT INTO QUESTION (id, created_time, contents, title, writer_id) VALUES (2, CURRENT_TIMESTAMP, '마스터의 테스트 질문', 'master 테스트 질문2', 1);
+INSERT INTO QUESTION (id, created_time, contents, title,count_of_answer, writer_id) VALUES (1, CURRENT_TIMESTAMP, '테스트 질문입니다', 'guest의 테스트 질문', 4 ,2);
+INSERT INTO QUESTION (id, created_time, contents, title,count_of_answer, writer_id) VALUES (2, CURRENT_TIMESTAMP, '마스터의 테스트 질문', 'master 테스트 질문2',0, 1);
 
 INSERT INTO ANSWER (id, created_time, contents, question_id, writer_id) VALUES (1, CURRENT_TIMESTAMP, '테스트 댓글 입니다1', 1, 1);
 INSERT INTO ANSWER (id, created_time, contents, question_id, writer_id) VALUES (2, CURRENT_TIMESTAMP, '테스트 댓글 입니다2', 1, 2);

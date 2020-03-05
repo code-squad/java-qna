4854730a55f5a73ab8b0bbbfdf7bef443cbed04f
Museop Kim <museopkim0214@gmail.com>
2020-03-05 18:10:03 +0900

    MvcConfig 추가
    로직이 없는 단순 매핑이 필요한 URL에 MvcConfig를 적용하여 View와 URL을 매핑 하였습니다.

---
+++ src/main/java/com/codesquad/qna/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.codesquad.qna.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        registry.addViewController("").setViewName("index");
+        registry.addViewController("/users/login").setViewName("user/login");
+        registry.addViewController("/users/form").setViewName("user/form");
+    }
+}

01a6874feb6077608eb90d16cfbe58a68fa39760
KIM DONG HWAN <quaint3@gmail.com>
2020-03-05 13:26:46 +0900

    Feat : User 에 생성, 수정 시각 추가
    기능 추가에 맞게, data.sql 데이터도 수정

--- src/main/java/com/codessquad/qna/domain/User.java
+++ src/main/java/com/codessquad/qna/domain/User.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class User extends BaseTimeEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO USER (id, user_id, password, name, email) VALUES (1, 'master', '1234', '관리자', 'master@gmail.com');
-INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'guest', '1234', '손님', 'guest@gmail.com');
+INSERT INTO USER (id, created_time, modified_time, user_id, password, name, email) VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'master', '1234', '관리자', 'master@gmail.com');
+INSERT INTO USER (id, created_time, modified_time, user_id, password, name, email) VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'guest', '1234', '손님', 'guest@gmail.com');
 
 INSERT INTO QUESTION (id, created_time, modified_time, contents, title,count_of_answer, writer_id) VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '테스트 질문입니다', 'guest의 테스트 질문', 4 ,2);
 INSERT INTO QUESTION (id, created_time, modified_time, contents, title,count_of_answer, writer_id) VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '마스터의 테스트 질문', 'master 테스트 질문2',0, 1);

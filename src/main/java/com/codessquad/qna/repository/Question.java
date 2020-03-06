4a3a96a7696ffa1056d330996c9b9edbf6c9a131
moonelysian <dandyblack@naver.com>
2020-03-06 14:47:00 +0900

    hashCode, equals override - 피드백 반영 - equals로 user id만 비교

--- src/main/java/com/codessquad/qna/repository/Question.java
+++ src/main/java/com/codessquad/qna/repository/Question.java
@@ -1,6 +1,8 @@
 package com.codessquad.qna.repository;
 
 
+import com.codessquad.qna.exception.CustomWrongFormatException;
+import com.codessquad.qna.util.PathUtil;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.lang3.ObjectUtils;
@@ -37,19 +39,47 @@ public class Question {
         this.writer = user;
     }
 
-    public void update(Question question) {
-        this.title = question.title;
-        this.contents = question.contents;
+    public void update(Question updateData) {
+        if (!isCorrectFormat(updateData)) {
+            throw new CustomWrongFormatException(PathUtil.BAD_REQUEST, "입력값을 모두 입력해주세요");
+        }
+        this.title = updateData.title;
+        this.contents = updateData.contents;
     }
 
     public boolean isCorrectFormat(Question question) {
         boolean titleIsExist = ObjectUtils.isNotEmpty(question.getTitle());
         boolean contentIsExist = ObjectUtils.isNotEmpty(question.getContents());
-        boolean writerIsExist = ObjectUtils.isNotEmpty(question.getWriter());
-        return titleIsExist && contentIsExist && writerIsExist;
+
+        return titleIsExist && contentIsExist;
     }
 
     public boolean isCorrectWriter(User user) {
-        return this.writer.getId().compareTo(user.getId()) == 0;
+        return this.writer.equals(user);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0: id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Question other = (Question) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }

e069ac1c671780da8a1df93ab01ffaee9db702ac
guswns1659 <guswns1659@gmail.com>
2020-03-08 18:56:33 +0900

    20.03.08) homeController 변경
    findByDeleted() -> findByDeletedFalse()

--- src/main/java/com/codessquad/qna/domain/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/domain/QuestionRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findAllByDeleted(boolean deleted);
+//    List<Question> findAllByDeleted(boolean deleted);
+    List<Question> findAllByDeletedFalse();
 }

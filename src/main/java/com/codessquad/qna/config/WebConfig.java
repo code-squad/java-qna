ed47127a10f88a407d78d22fbad65c5826ee4843
kyungrae <rlarudfo93@gmail.com>
2020-03-09 18:01:48 +0900

    feat: interceptor를 이용하여 http request 정보 기록
    preHandle 메소드와 afterCompletion 메소드 사이의 시간 차이를 통해 응답 속도를 기록했다.

---
+++ src/main/java/com/codessquad/qna/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.codessquad.qna.config;
+
+import com.codessquad.qna.interceptors.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor());
+    }
+}
---
+++ src/main/java/com/codessquad/qna/interceptors/LoggerInterceptor.java
@@ -0,0 +1,25 @@
+package com.codessquad.qna.interceptors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoggerInterceptor extends HandlerInterceptorAdapter {
+
+    Logger logger = LogManager.getLogger(LoggerInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        request.setAttribute("startTime", System.currentTimeMillis());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        long duration = System.currentTimeMillis() - (Long)request.getAttribute("startTime");
+        logger.info("{} {} {}ms", request.getMethod(), request.getRequestURL(), duration);
+    }
+}

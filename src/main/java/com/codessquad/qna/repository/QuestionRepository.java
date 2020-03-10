870d091e425596f7984fd75373d11da5e0130cdd
moonelysian <dandyblack@naver.com>
2020-03-10 15:14:00 +0900

    띄어쓰기, 안 쓰는 import 정리 - 보기 좋게 띄어쓰기 추가 - 안 쓰는 import 삭제

--- src/main/java/com/codessquad/qna/repository/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/repository/QuestionRepository.java
@@ -3,13 +3,12 @@ package com.codessquad.qna.repository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
 import javax.transaction.Transactional;
 import java.util.List;
 
 public interface QuestionRepository extends CrudRepository<Question, Long> {
     List<Question> findAllByDeletedFalse();
+
     @Modifying
     @Transactional
     @Query(value = "UPDATE Question SET deleted=true WHERE id=:id")

12906705cd30fdcbbb8fefe429c3b7a6df3a116a
David Chung <david215@berkeley.edu>
2020-03-11 23:40:39 +0900

    Implement user info update
    - Add a POST mapping for updating user info in UserController.
        - Verify that the provided password is correct.
        - If the password is correct, update user info and save to CrudRepository.
        - Else, return the update-failed page.
    - Modify input id in the update form to automate User bean initialization.

--- src/main/java/io/david215/forum/user/UserController.java
+++ src/main/java/io/david215/forum/user/UserController.java
@@ -44,17 +44,41 @@ public class UserController {
             model.addAttribute("user", user);
             return "user/profile";
         }
+        model.addAttribute("userId", userId);
         return "user/not-found";
     }
 
     @GetMapping("/users/{userId}/update")
-    public String update(Model model, @PathVariable String userId) {
+    public String updateForm(Model model, @PathVariable String userId) {
         Optional<User> optionalUser = userRepository.findByUserId(userId);
         if (optionalUser.isPresent()) {
             User user = optionalUser.get();
             model.addAttribute("user", user);
             return "user/update";
         }
+        model.addAttribute("userId", userId);
+        return "user/not-found";
+    }
+
+    @PostMapping("/users/{userId}")
+    public String update(Model model, User newUserInfo, String oldPassword) {
+        String userId = newUserInfo.getUserId();
+        Optional<User> optionalUser = userRepository.findByUserId(userId);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            String password = user.getPassword();
+            if (oldPassword.equals(password)) {
+                user.setPassword(newUserInfo.getPassword());
+                user.setName(newUserInfo.getName());
+                user.setEmail(newUserInfo.getEmail());
+                userRepository.save(user);
+                return "redirect:/users";
+            } else {
+                model.addAttribute("userId", userId);
+                return "user/update-failed";
+            }
+        }
+        model.addAttribute("userId", userId);
         return "user/not-found";
     }
 }

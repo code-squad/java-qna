975b249df4cd6db2fac61227a1d2206550a92d36
Byung Hyun Choi <choi8608@gmail.com>
2020-03-12 22:29:05 +0900

    refactor : data 초기 값 추가 설정
    - User 4, Contents 16 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,21 @@
-INSERT INTO USER (id, user_id, password, name, email) VALUES (1, 'javajigi', 'test', '자바지기', 'javajigi@slipp.net');
-INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
+INSERT INTO USER (id, user_id, password, name, email) VALUES (1, 'javajigi', '1111', '자바지기', 'javajigi@slipp.net');
+INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'sanjigi', '1111', '산지기', 'sanjigi@slipp.net');
+INSERT INTO USER (id, user_id, password, name, email) VALUES (3, 'badaigi', '1111', '바다지기', 'badajigi@slipp.net');
+INSERT INTO USER (id, user_id, password, name, email) VALUES (4, 'mooljigi', '1111', '물지기', 'mooljigi@slipp.net');
 
 INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (1, 'Content1', '2020-02-28T03:50:36.802145', '2020-02-28T03:50:36.802145', 'Subject1', 1, false);
-INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (2, 'Content2', '2020-02-28T03:55:36.802145', '2020-02-28T03:55:36.802145', 'Subject2', 1, false);
-INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (3, 'Content3', '2020-02-28T04:50:36.802145', '2020-02-28T04:50:36.802145', 'Subject3', 2, false);
-INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (4, 'Content4', '2020-02-28T06:50:36.802145', '2020-02-28T06:50:36.802145', 'Subject4', 2, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (2, 'Content2', '2020-02-28T03:55:36.802145', '2020-02-28T03:55:36.802145', 'Subject2', 2, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (3, 'Content3', '2020-02-28T04:50:36.802145', '2020-02-28T04:50:36.802145', 'Subject3', 3, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (4, 'Content4', '2020-02-28T06:50:36.802145', '2020-02-28T06:50:36.802145', 'Subject4', 4, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (5, 'Content5', '2020-02-28T03:50:36.802145', '2020-02-28T03:50:36.802145', 'Subject5', 4, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (6, 'Content6', '2020-02-28T03:55:36.802145', '2020-02-28T03:55:36.802145', 'Subject6', 3, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (7, 'Content7', '2020-02-28T04:50:36.802145', '2020-02-28T04:50:36.802145', 'Subject7', 2, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (8, 'Content8', '2020-02-28T06:50:36.802145', '2020-02-28T06:50:36.802145', 'Subject8', 1, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (9, 'Content9', '2020-02-28T03:50:36.802145', '2020-02-28T03:50:36.802145', 'Subject9', 1, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (10, 'Content10', '2020-02-28T03:55:36.802145', '2020-02-28T03:55:36.802145', 'Subject10', 2, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (11, 'Content11', '2020-02-28T04:50:36.802145', '2020-02-28T04:50:36.802145', 'Subject11', 3, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (12, 'Content12', '2020-02-28T06:50:36.802145', '2020-02-28T06:50:36.802145', 'Subject12', 4, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (13, 'Content13', '2020-02-28T03:50:36.802145', '2020-02-28T03:50:36.802145', 'Subject13', 4, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (14, 'Content14', '2020-02-28T03:55:36.802145', '2020-02-28T03:55:36.802145', 'Subject14', 3, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (15, 'Content15', '2020-02-28T04:50:36.802145', '2020-02-28T04:50:36.802145', 'Subject15', 2, false);
+INSERT INTO QUESTION (id, contents, created_date_time, last_modified_date_time, title, user_id, deleted) VALUES (16, 'Content16', '2020-02-28T06:50:36.802145', '2020-02-28T06:50:36.802145', 'Subject16', 1, false);

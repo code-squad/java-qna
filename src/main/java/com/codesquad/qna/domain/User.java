323024a5eebff54f68171c41e9df75df2bc59e5e
ragdoll <>
2020-03-14 03:25:51 +0900

    DB에 유저 정보 저장 기능 구현
    DB에 유저 정보를 저장하고 리스트로 목록을 불러오는 기능을 구현 하였습니다.
    - User 클래스 Entity 선언
    - UserRepository 추가, save, findAll 메서드로 저장 및 출력

---
+++ src/main/java/com/codesquad/qna/domain/User.java
@@ -0,0 +1,59 @@
+package com.codesquad.qna.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable=false, length=20)
+    private String userId;
+    private String password;
+    private String userName;
+    private String email;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() { return id; }
+
+    @Override
+    public String toString() {
+        return "userId : " + userId + ", userName : " + userName + ", password : " + password + ", Email : " + email + ", ID : " + id;
+    }
+
+}

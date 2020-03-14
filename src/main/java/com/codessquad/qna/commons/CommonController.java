cf92e264c94f9d028d380c805318ea08d4d478ea
Byung Hyun Choi <choi8608@gmail.com>
2020-03-14 22:33:33 +0900

    refactor : page 처리 개선
    - 페이지 이동을 구현하였습니다.

--- src/main/java/com/codessquad/qna/commons/CommonController.java
+++ src/main/java/com/codessquad/qna/commons/CommonController.java
@@ -11,7 +11,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -21,7 +25,7 @@ public class CommonController {
   private QuestionRepository questionRepository;
 
   /**
-   * Feat : 모든 Question 을 가져옵니다.
+   * Feat : 시작 페이지로 처음 글 15 개를 가져옵니다.
    * Desc : Get 처리를 위해 분리하였습니다.
    * Return : /welcome
    */
@@ -30,7 +34,35 @@ public class CommonController {
     Pageable firstPageWithTwoElements = PageRequest.of(0, 15, Sort.by("lastModifiedDateTime").descending());
     Page<Question> questions = questionRepository.findAllByDeleted(firstPageWithTwoElements, false);
     model.addAttribute("questions", questions.getContent());
-    model.addAttribute("pagesCount", questions.getTotalPages());
+
+    Map<Integer, String> pageCountMap = new LinkedHashMap<>();
+    for (int i = questions.getNumber(); i < questions.getTotalPages()
+                                        && i < questions.getNumber() + 5; i++) {
+      pageCountMap.put(i + 1, "http://localhost:8080/" + (i + 1));
+    }
+    model.addAttribute("pageCountMap", pageCountMap);
+
+    return "/welcome";
+  }
+
+  /**
+   * Feat : pageCount 를 받아 해당 페이지의 글을 가져옵니다.
+   * Desc : Get 처리를 위해 분리하였습니다.
+   * Return : /welcome
+   */
+  @GetMapping("/{pageCount}")
+  public String welcomeGetWithPageCount(@PathVariable String pageCount, Model model) {
+    Pageable firstPageWithTwoElements =
+        PageRequest.of(Integer.parseInt(pageCount) - 1, 15, Sort.by("lastModifiedDateTime").descending());
+    Page<Question> questions = questionRepository.findAllByDeleted(firstPageWithTwoElements, false);
+    model.addAttribute("questions", questions.getContent());
+
+    Map<Integer, String> pageCountMap = new LinkedHashMap<>();
+    for (int i = questions.getNumber(); i < questions.getTotalPages() && i < questions.getNumber() + 5; i++) {
+      pageCountMap.put(i + 1, "http://localhost:8080/" + (i + 1));
+    }
+    model.addAttribute("pageCountMap", pageCountMap);
+
     return "/welcome";
   }
 

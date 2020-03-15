cffea09b6584dfdecc349fd7f2d9bb481321949d
suhyunsim <cnc1106@naver.com>
2020-03-15 23:20:33 +0900

    Feat: AnswerRepository 생성
    JpaRepository를 상속받은 AnswerRepository를 생성한다.
    질문 아이디로 댓글을 찾을 수 있도록 findByQuestionId를 추가한다.

---
+++ src/main/java/com/codessquad/domain/qna/AnswerRepository.java
@@ -0,0 +1,9 @@
+package com.codessquad.domain.qna;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionId(Long id);
+}

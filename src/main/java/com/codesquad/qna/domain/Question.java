ef859ae323a5be5c46d30f179116fa8a68592ab9
ragdoll <>
2020-03-16 00:46:40 +0900

    질문 등록하기 기능 구현
    질문 등록하기 기능을 구현 하였습니다.
    - Question을 DB와 연동 (PK : id)
    - Date 타입의 생성 날짜 정보를 LocalDateTime으로 수정
    - 질문 등록시 index로 redirect -> 질문 목록 자동 출력

---
+++ src/main/java/com/codesquad/qna/domain/Question.java
@@ -0,0 +1,67 @@
+package com.codesquad.qna.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String writer;
+    private String title;
+    private String contents;
+
+    @Column(name="CREATED_DATE")
+    private LocalDateTime createdDate;
+
+    public Question() {
+        createdDate = LocalDateTime.now();
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Writer : " + writer + "\n" +
+                "Title : " + title + "\n" +
+                "Contents : " + contents + "\n" +
+                "Date : " + getCreatedDate() + "\n" +
+                "Id : " + id;
+    }
+}

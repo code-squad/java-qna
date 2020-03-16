857791692e3eba8083ca3dfeffa74d089e5f5221
ragdoll <>
2020-03-16 20:55:58 +0900

    로그인에 따른 뷰 변환 기능 구현
    유저의 세션 유무에 따라 뷰를 다르게 출력하는 기능을 구현 하였습니다.
    - handlebars의 unless (if not) 활용
    - 유저 정보가 있을 경우(로그인 상태) 로그아웃, 개인정보수정 메뉴 출력
    - 유저 정보가 없을 경우(로그아웃 상태) 로그인, 회원 가입 메뉴 출력

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER (ID, USER_ID, PASSWORD, USER_NAME, EMAIL) VALUES ('1', 'hello', 'hello', 'hello', 'hello@codesquad.kr');
+INSERT INTO USER (ID, USER_ID, PASSWORD, USER_NAME, EMAIL) VALUES ('2', 'hi', 'hi', 'hi', 'hi@codesquad.kr');

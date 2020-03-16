272cc3fd2674393caa76f3988a5a66c2eaab03ba
guswns1659 <guswns1659@gmail.com>
2020-03-16 18:01:50 +0900

    20.03.16) PageController 구현

--- src/main/java/com/codessquad/qna/HomeController.java
+++ src/main/java/com/codessquad/qna/HomeController.java
@@ -1,19 +1,31 @@
 package com.codessquad.qna;
 
+import com.codessquad.qna.domain.Question;
 import com.codessquad.qna.domain.QuestionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @Autowired
     private QuestionRepository questionRepository;
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("questions", questionRepository.findAllByDeletedFalse());
+        PageRequest pageRequest = PageRequest.of(1, 5, Sort.by("createdDate").descending());
+        Page<Question> pages = questionRepository.findAll(pageRequest);
+        model.addAttribute("questions", questionRepository.findAll(pageRequest));
+        model.addAttribute("countOfPages", pages.getContent());
+        logger.info("pages.getContent : {}" , pages.getContent().size());
         return "index";
     }
 

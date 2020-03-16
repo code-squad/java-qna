6980aa82b6f07d38f7ab9a58a5b49bc11ecd5ce9
haveagood@naver.com <haveagood@naver.com>
2020-03-16 21:31:37 +0900

    <feat> : 질문에 답변 기능 추가
    - 질문에 답변을 달 수 있도록 Answer Class, Repository, Controller 추가하였습니다.
    
    - Answer는 Question에 종속적이기 때문에 URL 매핑을 question/{id}에 종속적이게 만들었습니다.
    
    - 작성자와 질문에 답변이 여러개 달릴 수 있기 때문에 ManyToOne 설정을 해주었습니다.
    
    - Lob 어노테이션을 통해서 Contents의 범위를 확장했습니다.
    
    # 꼬릿말은 아래에 작성 : ex) #이슈 번호
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

---
+++ src/main/java/com/codessquad/qna/Answer.java
@@ -0,0 +1,90 @@
+package com.codessquad.qna;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_writer"))
+    private User writer;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_to_question"))
+    private Question question;
+
+    @Lob
+    private String contents;
+    private LocalDateTime writtenTime;
+
+    public Answer() {
+
+    }
+
+    public Answer(User writer, Question question, String contents) {
+        this.writer = writer;
+        this.question = question;
+        this.contents = contents;
+        this.writtenTime = LocalDateTime.now();
+    }
+
+    public String getWrittenTime() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String writtenTimeToString = writtenTime.format(dateTimeFormatter);
+        return writtenTimeToString;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getWriter() {
+        return writer;
+    }
+
+    public void setWriter(User writer) {
+        this.writer = writer;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Answer answer = (Answer) o;
+        return Objects.equals(id, answer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", writer=" + writer +
+                ", contents='" + contents + '\'' +
+                ", createDate=" + writtenTime +
+                '}';
+    }
+}

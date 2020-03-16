777c74572a373780c80538f9a6386195661cb14c
wooody92 <pkower4@naver.com>
2020-03-16 19:48:36 +0900

    add page-utils class
    페이지 생성 및 인자를 관리하는 클래스 생성했습니다. 페이지 관련 기능을 테스트하기 위해 테스트 메서드 생성했습니다.

--- src/main/java/com/codessquad/qna/HomeController.java
+++ src/main/java/com/codessquad/qna/HomeController.java
@@ -1,9 +1,15 @@
 package com.codessquad.qna;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -12,7 +18,22 @@ public class HomeController {
 
     @GetMapping("/")
     public String viewQuestionList(Model model) {
-        model.addAttribute("questions", questionRepository.findAll());
+        List<Page> pages = createPages();
+        int pageNumber; // @PathValuable 로 인덱스 가져오기
+        model.addAttribute("questions", pages.get(0));
         return "/index";
     }
+
+    public List<Page> createPages() {
+        int lastPage = 3;
+        List<Page> pages = new ArrayList<>();
+        PageRequest pageRequest;
+
+        for (int pageNumber = 0; pageNumber < lastPage; pageNumber++) {
+            pageRequest = PageRequest.of(pageNumber, 5);
+            pages.add(questionRepository.findAll(pageRequest));
+        }
+        return pages;
+    }
 }
+

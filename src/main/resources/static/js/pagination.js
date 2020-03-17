d93bb4f8938db8a6fcb07a65796fe565db1b342d
moonelysian <dandyblack@naver.com>
2020-03-17 17:20:11 +0900

    pagination 수정 - 1을 누르면 가장 최근에 쓴 질문 15개가 보이고, 2를 누르면 16번째부터 30번까지의 글이 보이게 함 -  화면에 보이는 페이지 수가 5개 초과인 경우 다음 페이지 그룹으로 이동할 수 있는 버튼이 나타나게 함

--- src/main/resources/static/js/pagination.js
+++ src/main/resources/static/js/pagination.js
@@ -1,10 +1,12 @@
 const FIRST_PAGE = 1;
+const NUMBER_OF_BUTTON = 5;
 let LAST_PAGE;
+let section = 1;
+let pageNavSection = document.querySelector('.pagination');
 
 document.addEventListener('DOMContentLoaded', async function() {
     await changePage(FIRST_PAGE);
-    let pageNavSection = document.querySelector('.pagination');
-    makePageNaveBar(pageNavSection);
+    makePageNaveBar();
 });
 
 const changePage = async (pageNumber) => {
@@ -19,13 +21,41 @@ const changePage = async (pageNumber) => {
     appendQuestionList(questionList, questionSection);
 };
 
-const makePageNaveBar = (pageNavSection) => {
-    pageNavSection.innerHTML = pageNavSection.innerHTML + `<li onclick="changePage(${FIRST_PAGE})"><a class="page-link">«</a></li>`
-    for (let pageNumber = FIRST_PAGE; pageNumber <= LAST_PAGE; pageNumber++) {
-        pageNavSection.innerHTML = pageNavSection.innerHTML + `<li onclick="changePage(${pageNumber})"><a class="page-link">${pageNumber}</a></li>`
+const changePageNavSection = (command) => {
+    if (command === 'up') {
+        section = Math.min(section+1, Math.ceil(LAST_PAGE/5));
+        makePageNaveBar();
+        return;
     }
-    pageNavSection.innerHTML = pageNavSection.innerHTML + `<li onclick="changePage(${LAST_PAGE})"><a class="page-link">»</a></li>`
-};
+
+    if (command === 'down') {
+        section = Math.max(1, section-1);
+        makePageNaveBar();
+        return;
+    }
+}
+
+const makePageNaveBar = () => {
+    document.querySelectorAll(".page-button").forEach(el => el.remove());
+
+    if (LAST_PAGE <= NUMBER_OF_BUTTON) {
+        appendPageButton(FIRST_PAGE, LAST_PAGE);
+        return;
+    }
+
+    const lastButton = Math.min(section * NUMBER_OF_BUTTON, LAST_PAGE);
+    const startButton = (section-1) * NUMBER_OF_BUTTON + 1;
+
+    pageNavSection.innerHTML = pageNavSection.innerHTML + `<li class="page-button" onclick="changePageNavSection('down')"><a class="page-link">«</a></li>`
+    appendPageButton(startButton, lastButton);
+    pageNavSection.innerHTML = pageNavSection.innerHTML + `<li class="page-button" onclick="changePageNavSection('up')"><a class="page-link">»</a></li>`
+}
+
+const appendPageButton = (startButton, lastButton) => {
+    for (let pageNumber = startButton; pageNumber <= lastButton; pageNumber++) {
+        pageNavSection.innerHTML = pageNavSection.innerHTML + `<li class="page-button" onclick="changePage(${pageNumber})"><a class="page-link">${pageNumber}</a></li>`
+    }
+}
 
 const appendQuestionList = (questionList, questionSection) => {
     document.querySelectorAll(".question").forEach(e => e.remove());

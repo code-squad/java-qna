2d65754ae6d23d9642738f28acdd3ca076fc944a
Ever <hsik0225@gmail.com>
2020-03-17 22:55:45 +0900

    [수정] : MainController Repository 에서 Service로 수정

--- src/main/java/com/codesquad/qna/web/MainController.java
+++ src/main/java/com/codesquad/qna/web/MainController.java
@@ -1,6 +1,6 @@
 package com.codesquad.qna.web;
 
-import com.codesquad.qna.repository.QuestionRepository;
+import com.codesquad.qna.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @Autowired
-    private QuestionRepository questionRepository;
+    private QuestionService questionService;
 
     @GetMapping("/")
     public String showMain(Model model) {
-        model.addAttribute("questions", questionRepository.findAll());
+        model.addAttribute("questions", questionService.findAll());
         return "main";
     }
 }

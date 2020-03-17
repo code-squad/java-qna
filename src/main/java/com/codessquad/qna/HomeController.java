bcadc8e8613ad68bbad12768595b42df365a1df2
guswns1659 <guswns1659@gmail.com>
2020-03-17 12:10:55 +0900

    20.03.17) 한 코드 복사 하기 전 커밋

--- src/main/java/com/codessquad/qna/HomeController.java
+++ src/main/java/com/codessquad/qna/HomeController.java
@@ -1,13 +1,11 @@
 package com.codessquad.qna;
 
-import com.codessquad.qna.domain.Question;
 import com.codessquad.qna.domain.QuestionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,26 +19,21 @@ public class HomeController {
     @Autowired
     private QuestionRepository questionRepository;
     private List<Page> pages;
-    private static final int SIZE_OF_QUESTIONS_OF_PAGE = 15;
+    private static final int QUESTIONS_OF_EACHPAGE = 15;
 
     @GetMapping("/")
     public String index(Model model) {
         pages = createPages();
-        model.addAttribute("questions", questionRepository.findAll());
+        model.addAttribute("questions", pages.get(0));
         model.addAttribute("pages", pages);
         return "index";
     }
 
-    @GetMapping("/test1")
-    public Page viewQuestionList() {
-        pages = createPages();
-        return pages.get(0);
-    }
 
     private List<Page> createPages() {
         int numberOfQuestions = questionRepository.findAll().size();
-        int lastPage = numberOfQuestions / SIZE_OF_QUESTIONS_OF_PAGE;
-        List<Page> pages = new ArrayList<>();
+        int lastPage = (numberOfQuestions / QUESTIONS_OF_EACHPAGE) + 1;
+        pages = new ArrayList<>();
         PageRequest pageRequest;
 
         for (int i = 0; i < lastPage; i++) {
--- src/main/java/com/codessquad/qna/PageController.java
+++ src/main/java/com/codessquad/qna/PageController.java
@@ -1,35 +1,44 @@
-//package com.codessquad.qna;
-//
-//import com.codessquad.qna.domain.QuestionRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@RestController
-//public class PageController {
-//    @Autowired
-//    private QuestionRepository questionRepository;
-//
-//    @GetMapping("/test1")
-//    public Page viewQuestionList() {
-//        List<Page> pages = createPages();
-//        return pages.get(0);
-//    }
-//
-//    private List<Page> createPages() {
-//        int lastPage = 3;
-//        List<Page> pages = new ArrayList<>();
-//        PageRequest pageRequest;
-//
-//        for (int i = 0; i < lastPage; i++) {
-//            pageRequest = PageRequest.of(i, 5);
-//            pages.add(questionRepository.findAll(pageRequest));
-//        }
-//        return pages;
-//    }
-//}
+package com.codessquad.qna.controller;
+
+import com.codessquad.qna.domain.Question;
+import com.codessquad.qna.domain.QuestionRepository;
+import com.codessquad.qna.web.PageUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
+
+@Controller
+public class PageController {
+
+    private static Logger log = LoggerFactory.getLogger(PageController.class);
+    private static final int PAGE_SIZE = 4;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @GetMapping(value = "/")
+    public String index(Model model, Optional<Integer> page) {
+        int pageNumber = page.isPresent() ? page.get() - 1 : 0;
+
+        Page<Question> questions = questionRepository
+                .findAll(PageRequest.of(pageNumber, PAGE_SIZE, Direction.DESC, "id"));
+
+        int current = questions.getNumber() + 1;
+        int totalPages = questions.getTotalPages();
+        int index = questions.getNumber();
+
+        model.addAttribute("questions", questions.getContent());
+        model.addAttribute("pageUtil",
+                new PageUtil(current, index, totalPages));
+        return "index";
+    }
+
+}

beba14128846b03c8d5de9a827ce9ffc4fcee0de
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-20 01:39:54 +0900

    [MODIFY] AuthController to delete duplicated codes and implement custom exceptions

--- src/main/java/com/codessquad/qna/controller/auth/AuthController.java
+++ src/main/java/com/codessquad/qna/controller/auth/AuthController.java
@@ -2,6 +2,7 @@ package com.codessquad.qna.controller.auth;
 
 import com.codessquad.qna.controller.users.UsersRepository;
 import com.codessquad.qna.domain.Users;
+import com.codessquad.qna.exception.UnauthorizedException;
 import com.codessquad.qna.utils.PathUtil;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +19,7 @@ public class AuthController {
   @PostMapping("/login") //@RestController에서 하면 리다이렉트가 안된다고 한다. 왜인지는 모르겠다.
   public String login(String userId, String password, HttpSession session) {
     Users user = usersRepository.findByUserId(userId);
-    if (user == null) {
-      return PathUtil.USERS_LOGIN_FAILED;
-    }
-    if (!user.matchPassword(password)) {
+    if (user == null || !user.matchPassword(password)) {
       return PathUtil.USERS_LOGIN_FAILED;
     }
     session.setAttribute("sessionUser", user);
@@ -30,8 +28,10 @@ public class AuthController {
 
   @GetMapping("/logout")
   public String usersLogout(HttpSession httpSession) {
+    if (httpSession.getAttribute("sessionUser") == null) {
+      throw new UnauthorizedException(PathUtil.REDIRECT_TO_USERS_LOGIN, "no one is logged in yet.");
+    }
     httpSession.removeAttribute("sessionUser");
-    System.out.println("logout succeed");
     return PathUtil.REDIRECT_TO_MAIN;
   }
 }

82700b57b2bb6ed19015aac37d8b8a4492b0784f
wooody92 <pkower4@naver.com>
2020-03-20 14:31:42 +0900

    debug move-page error
    HomeController와 PageController 나누었을 때 좌,우 버튼 클릭 시 발생하는 에러 제거했습니다. movewNextPage, movePrePage가 HomeController와 다른 클래스에 있어서 에러 발생했습니다.

--- src/main/java/com/codessquad/qna/HomeController.java
+++ src/main/java/com/codessquad/qna/HomeController.java
@@ -16,16 +16,16 @@ public class HomeController {
     private static final int INITIAL_PAGE_NUMBER = 0;
     private static final int DEFAULT_FIRST_PAGE = 1;
     private static final int DEFAULT_LAST_PAGE = 6;
+    PageUtils pageUtils = new PageUtils(DEFAULT_FIRST_PAGE, DEFAULT_LAST_PAGE);
 
-    @GetMapping("/")
-    public String viewWelcomePage(Model model) {
-        PageUtils pageUtils = new PageUtils(DEFAULT_FIRST_PAGE, DEFAULT_LAST_PAGE);
-        pageUtils.initPage(questionRepository);
-        List<Page> pages = pageUtils.createPages(questionRepository);
-        List<Page> subPages = pageUtils.getSubPages(questionRepository);
-
-        model.addAttribute("pages", subPages);
-        model.addAttribute("questions", pages.get(INITIAL_PAGE_NUMBER));
-        return "/index";
-    }
+//    @GetMapping("/")
+//    public String viewWelcomePage(Model model) {
+//        pageUtils.initPage(questionRepository);
+//        List<Page> pages = pageUtils.createPages(questionRepository);
+//        List<Page> subPages = pageUtils.getSubPages(questionRepository);
+//
+//        model.addAttribute("pages", subPages);
+//        model.addAttribute("questions", pages.get(INITIAL_PAGE_NUMBER));
+//        return "/index";
+//    }
 }
--- src/main/java/com/codessquad/qna/PageController.java
+++ src/main/java/com/codessquad/qna/PageController.java
@@ -14,9 +14,21 @@ public class PageController {
     @Autowired
     private QuestionRepository questionRepository;
 
+    private static final int INITIAL_PAGE_NUMBER = 0;
     private static final int DEFAULT_FIRST_PAGE = 1;
     private static final int DEFAULT_LAST_PAGE = 6;
     PageUtils pageUtils = new PageUtils(DEFAULT_FIRST_PAGE, DEFAULT_LAST_PAGE);
+
+    @GetMapping("/")
+    public String viewWelcomePage(Model model) {
+        pageUtils.initPage(questionRepository);
+        List<Page> pages = pageUtils.createPages(questionRepository);
+        List<Page> subPages = pageUtils.getSubPages(questionRepository);
+
+        model.addAttribute("pages", subPages);
+        model.addAttribute("questions", pages.get(INITIAL_PAGE_NUMBER));
+        return "/index";
+    }
 
     @GetMapping("/{pageNumber}")
     public String viewQuestionsOnEachPage(@PathVariable int pageNumber, Model model) {
@@ -34,7 +46,7 @@ public class PageController {
         return "redirect:/" + pageUtils.plusPageCount();
     }
 
-    @GetMapping("/movePrev")
+    @GetMapping("/movePre")
     public String movePrePage() {
         return "redirect:/" + pageUtils.minusPageCount();
     }
